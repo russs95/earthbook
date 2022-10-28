635be3d241948673c8a65f329532907e283dee5c
russmaier <russmaier@gmail.com>
2022-10-28 08:52:11 +0800

    up all

--- index-javascripts.js
+++ index-javascripts.js
@@ -11,22 +11,35 @@ function scrollFunction() {
     
    
      document.getElementById("earthbook-navbar").style.height = "70px";
-     document.getElementById("book-title").style.display = "none";
-     document.getElementById("side").style.marginTop = "-100px";
-   
-    document.getElementById("book-header-logo").style.display = "block"; 
-     document.getElementById("book-header-logo").style.width = "160px";
+     document.getElementById("main-title").style.opacity = "0";
+     document.getElementById("sub-title").style.display = "none";
+     document.getElementById("sub-earthbook").style.display = "none";
+     document.getElementById("side").style.marginTop = "-100px"
+     document.getElementById("book-header-logo").style.opacity = "1"; 
+     //document.getElementById("book-header-logo").style.width = "160px";
     document.getElementById("book-header-logo").style.height = "60px";
-    document.getElementById("book-header-logo").style.margin = "auto";
+    document.getElementById("book-header-logo").style.top = "5px";
+    
+    //document.getElementById("book-header-logo").style.margin = "auto";
+           
+    //document.getElementById("book-header-logo").style.marginBottom = "0"; 
     //document.getElementById("mandala-day").style.background = "url('../svgs/tractatus-simple-mandala.svg') center no-repeat";
-    //document.getElementById("mandala-day").style.backgroundSize = "contain";
+     
  
    } else {
      //BEFORE
-     document.getElementById("earthbook-navbar").style.height = "200px";
-    document.getElementById("book-title").style.display = "block";
-     document.getElementById("book-header-logo").style.display = "none";
+     document.getElementById("earthbook-navbar").style.height = "208px";
+     document.getElementById("book-header-logo").style.opacity = "0";
      document.getElementById("side").style.marginTop = "0px";
+     document.getElementById("main-title").style.opacity = "1";
+    //document.getElementById("book-title").style.height = "unset";
+     //document.getElementById("book-header-logo").style.marginTop = "90px";  
+    // document.getElementById("book-title").style.marginTop = "-70px";   
+     document.getElementById("main-title").style.display = "block";
+     document.getElementById("sub-title").style.display = "block";
+     document.getElementById("sub-earthbook").style.display = "block";
+    document.getElementById("book-header-logo").style.top = "50px";  
+   
  
    }
  }
