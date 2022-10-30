3d54ae3b0883d56dc7033a2fcd8b83607bbc45f6
russmaier <russmaier@gmail.com>
2022-10-30 14:58:07 +0800

    update

--- en/includes/buy-curtain.js
+++ en/includes/buy-curtain.js
@@ -20,8 +20,8 @@ class BuyCurtain extends HTMLElement {
                   <img src="../photos/cover-epub-400px.webp" alt="The Epub version of the book is best for iphone and android ereaders." title="The Epub version of the book is best for iphone and android ereaders.">
 
                   <div class="book-buy-info">
-                  <div class="buy-book-info-title">Ebook Edition</div>
-                  <div class="buy-book-info-text">The optimal reading format for iPhones and Android e-Book apps.</div>
+                  <div class="buy-book-info-title"><h4>Ebook Edition</h4></div>
+                  <div class="buy-book-info-text"><h6>The optimal reading format for iPhones and Android e-Book apps.</h6></div>
 
                     <a href="paypal" class="action-btn" style="cursor:pointer; width:90%;"><i style="background-image: url(../icons/buy.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 5px;"></i>10$</a>
 
@@ -35,9 +35,9 @@ class BuyCurtain extends HTMLElement {
                 <div class="book-listing">
                   <img src="../photos/cover-print-400px.webp"  alt="The print version of the book" title="The print version of the book">
                   <div class="book-buy-info">
-                  <div class="buy-book-info-title">Print Edition</div>
+                  <div class="buy-book-info-title"><h4>Print Edition</h4></div>
 
-                  <div class="buy-book-info-text">The print version of the book is on its way.  Preorder here.</div>
+                  <div class="buy-book-info-text"><h6>The print version of the book is on its way.  Preorder here.</h6></div>
 
                     <a href="paypal" class="action-btn" style="cursor:pointer; width:90%;"><i style="background-image: url(../icons/buy.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 5px;"></i>10$</a>
 
