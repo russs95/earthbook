e1eb1c07c53f87e9e9cf0a482c5cb0075100134c
russmaier <russmaier@gmail.com>
2022-11-05 19:34:34 +0800

    go

--- content-javascript.js
+++ content-javascript.js
@@ -2,14 +2,14 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 
-  if ( document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ) {
+  if ( document.body.scrollTop > 40 || document.documentElement.scrollTop > 40 ) {
     
     //AFTERmargin-top: -67px;
     document.getElementById("earthbook-navbar").style.height = "60px";
     document.getElementById("ct-chapter-title").style.fontSize = "1.0em";
     document.getElementById("ct-book-title").style.fontSize = "0.66em";
     document.getElementById("ct-book-title").style.marginTop = "0px";
-    document.getElementById("ct-chapter-top").style.marginTop = "-73px";    
+    document.getElementById("ct-chapter-top").style.marginTop = "-50px";    
     document.getElementById("ct-chap-location").style.display = "none";
     document.getElementById("ct-word-count").style.display = "none";
     document.getElementById("book-header-logo").style.display = "none";
