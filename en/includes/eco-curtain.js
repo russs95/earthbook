a72659a5967c4b1efe33393f4d5ba53b4e997bc9
russmaier <russmaier@gmail.com>
2022-11-10 21:58:52 +0800

    update eco

--- en/includes/eco-curtain.js
+++ en/includes/eco-curtain.js
@@ -12,28 +12,25 @@ class EcoCurtain extends HTMLElement {
                 <img src="../icons/eco.svg"  width="170px" height="170px" title="Ecological Accounting Kept" alt="Ecological Accounting Kept">
             </div>
             
-            <div class="text">
                 <h1>Earthbooks are Green.</h1>
                 <h3>An Earthbook is designed from the ground up to track, disclose and account for its net-green impact.</h3>
-            </div>
 
             <div>
                 <p width="65%">Throughout the enterprise of writing and publishing an Earthbook impact, the carbon, plastic and biodiversity impacts were tracked, accounted and annually disclosed.  See the full <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank">Regenerativity Report.</a></p>
             </div>
 
-            <div id="virtue-box" width="65%">
-                <div <div class="virtue">
+            <div>
                 <a href="about"><img src="https://ecobricks.org/banners/forearth-dark-350px.png" width="325px" height="" alt="Learn more about our Earth Enterprise structure" title="Learn more about our Earth Enterprise structure" loading="lazy"></a>
                 </div>
 
-                <div class="virtue">
+                <div>
                     <a href="https://www.gobrik.com/#my-catalyst/enterprise-disclosure/5b8c2b0922ddfb2591bf67be/" target="_blank"><img src="https://ecobricks.org/banners/762-disclose-light-350px.png" width="325px" height="" alt="Proudly Net-Green: We track, disclose and accelerate our plastic transition. View our 2022 Report." title="Click through to see a our 2022 Plastic Transition Report" loading="lazy"></a>
                 </div>
             </div>
 
-            <div id="wcb" class="carbonbadge wcb-d"></div>
-            
             <div><h2><i>The Medium is the Message.</i></h2></div>
+
+            <div id="wcb" class="carbonbadge wcb-d"></div>
 	      
             <span style="cursor:pointer" onclick="closeEco()">
              <div id="bottom-close-button">
