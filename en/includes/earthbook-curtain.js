4d86994e03cdb5b24704ebd82f69f57c70b2dcf5
russmaier <russmaier@gmail.com>
2022-11-13 15:26:42 +0800

    shabangg!

--- en/includes/earthbook-curtain.js
+++ en/includes/earthbook-curtain.js
@@ -17,19 +17,19 @@ class Earthbook extends HTMLElement {
 
             <div id="virtue-box">
 
-                <div class="virtue"><img src="../svgs/icon-opensource.svg" width="100%" height="100%" title="Open Source Code" alt="Open Source Code" style="cursor:pointer" onclick="openOS()"><div id="OS" class="virtue-desc">The EarthBook source code is open to review, improvement and forking <a href="https://github.com/russs95/earthbook">on Github</a>.  This means that publishing an Earthbook can be done by anyone anywhere.</div>
+                <div class="virtue"><img src="../svgs/icon-opensource.svg" width="100%" height="100%" title="EarthBook code is fully open-source" alt="EarthBook code is fully open-source" style="cursor:pointer" onclick="openOS()"><div id="OS" class="virtue-desc">The EarthBook source code is open to review, improvement and forking <a href="https://github.com/russs95/earthbook">on Github</a>.  This means that publishing an Earthbook is open to anyone anywhere.</div>
                 </div>
 
-                <div class="virtue"><img src="../svgs/icon-biodiversity.svg"  width="100%" height="100%" title="Supporting Biodiveristy" alt="Supporting Biodiversity" style="cursor:pointer" onclick="openBio()"><div id="bio" class="virtue-desc">During the writing and publishing of this book, the species supported in the enterprise physical space was tracked and accounted for.  <a href="https://russs.net/forest" target="_blank">See the publisher's full life list.</a></div>
+                <div class="virtue"><img src="../svgs/icon-biodiversity.svg"  width="100%" height="100%" title="Supporting, Accountin & Tracking Biodiveristy" alt="Supporting, Accountin & Tracking Biodiveristy" style="cursor:pointer" onclick="openBio()"><div id="bio" class="virtue-desc">During the writing and publishing of this book, the species supported by the enterprise's physical space was tracked and accounted.  <a href="https://russs.net/forest" target="_blank">See the publisher's full life list.</a></div>
                 </div>
 
-                <div class="virtue"><img src="../svgs/icon-ecoaccounting.svg"  width="100%" height="100%" title="Ecological Accounting Kept" alt="Ecological Accounting Kept" style="cursor:pointer" onclick="openAcc()"><div id="acc" class="virtue-desc">Throughout the enterprise of writing and publishing an Earthbook impac, the carbon, plastic and biodiversity impacts were tracked, accounted and annually disclosed.  See the full <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank">Regenerativity Report.</a></div>
+                <div class="virtue"><img src="../svgs/icon-ecoaccounting.svg"  width="100%" height="100%" title="Ecological Accounting Disclosure" alt="Ecological Accounting Disclosure" style="cursor:pointer" onclick="openAcc()"><div id="acc" class="virtue-desc">Throughout the enterprise of writing and publishing an Earthbook impac, the carbon, plastic and biodiversity impacts were tracked, accounted and annually disclosed.  See the full <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank">Regenerativity Report.</a></div>
                 </div>
 
-                <div class="virtue"><img src="../svgs/icon-for-earth.svg"  width="100%" height="100%" title="For-Earth Enterprise" alt="For-earth Enterprise"  onclick="openForE()"><div id="fore" class="virtue-desc">The intention and structure of this publishing enterprise is not-for-profit, for-Earth benefit.</div>
+                <div class="virtue"><img src="../svgs/icon-for-earth.svg"  width="100%" height="100%" title="For-Earth Enterprise" alt="For-Earth Enterprise"  onclick="openForE()"><div id="fore" class="virtue-desc">The intention and structure of this publishing enterprise is not-for-profit, for-Earth benefit.</div>
                 </div>
 
-                <div class="virtue"><img src="../svgs/icon-carbonseq.svg"  width="100%" height="100%" title="Net-Subtractive Carbon Impact" alt="Net-Subtractive Carbon Impact"  onclick="openCo2()"><div id="co2" class="virtue-desc">Crafted from fully open and non-proprietary web protocols, an Earthbook has the lightest of all digital footprints compared to other eReader formats. Ultra-light, fast-loading, and off-line cacheable so too is its digital emissions footprint minimized.  <span style="cursor:pointer; text-decoration:underline;" onclick="openEco()">Live impact test.</span></div>
+                <div class="virtue"><img src="../svgs/icon-carbonseq.svg"  width="100%" height="100%" title="Net-Subtractive Carbon Impact" alt="Net-Subtractive Carbon Impact"  onclick="openCo2()"><div id="co2" class="virtue-desc">Crafted from fully open and non-proprietary web protocols, an Earthbook has the lightest digital footprint compared to other eReader formats. Thanks to its byte-optimization and offline cacheability, an Earthbook's digital emissions footprint is in the top 10% of all websites.  <span style="cursor:pointer; text-decoration:underline;" onclick="openEco()">View live CO2e audit.</span></div>
                 </div>
 
                 <div class="virtue"><img src="../svgs/icon-creativecommons.svg"  width="100%" height="100%" title="Creative Commons" alt="Creative Commons License"  onclick="openCc()"><div id="cc" class="virtue-desc">The code and content of this book are licensed under a Creative Commons BY-SA-ND 4.0 License. <a href="https://github.com/russs95/earthbook/" target="_blank"> Access the Github repository.</a></div>
@@ -38,7 +38,7 @@ class Earthbook extends HTMLElement {
                 <div class="virtue"><img src="../svgs/icon-readfreely.svg"  width="100%" height="100%" title="Read Freely" alt="Read Freely"  onclick="openFree()"><div id="free" class="virtue-desc">This book is available to read <a href="preamble.html">freely online</a> and as a <a href="https://files.earthen.io/s/ZjS9TgWk5Tr7b4q" target="_blank">free downloadable PDF</a>.</div>
                 </div>
  
-                <div class="virtue"><img src="../svgs/icon-revenue.svg"  width="100%" height="100%" title="Support Independent Publishing" alt="Support Independent Publishing" onclick="openRev()"><div id="rev" class="virtue-desc">Earthbooks are 100% independent of corporate middlemen. 97% of the proceeds of the <span style="cursor:pointer; text-decoration:underline;" onclick="openBuy()">sales of book sales</span> go to the authors.</div>
+                <div class="virtue"><img src="../svgs/icon-revenue.svg"  width="100%" height="100%" title="Support Independent Publishing" alt="Support Independent Publishing" onclick="openRev()"><div id="rev" class="virtue-desc">Earthbooks are 100% independent of corporate middlemen. 97% of the proceeds of <span style="cursor:pointer; text-decoration:underline;" onclick="openBuy()">Ebook sales</span> go to the authors.</div>
                 </div>
 
                 <div class="virtue"><img src="../svgs/icon-accessibility.svg"  width="100%" height="100%" title="Made as human-friendly as possible" alt="Accessibility Maximized"  style="cursor:pointer" onclick="openMax()"><div id="max" class="virtue-desc">Earthbooks are designed from the ground up to be easy on the eyes, accessible, and multi-lingual.  <span style="cursor:pointer; text-decoration:underline;" onclick="openSettings()">Try adjusting the Earthbook's settings.</span></div>
@@ -48,39 +48,40 @@ class Earthbook extends HTMLElement {
 
             <div id="virtue-box-mobile">
 
-              <div class="virtue" style="cursor:pointer" onclick="openOS()"><img src="../svgs/icon-opensource.svg" width="100%" height="100%" title="Open Source" alt="Open Source"><div class="virtue-desc">The EarthBook source code is open to review, improvement and forking <a href="https://github.com/russs95/earthbook">on Github</a>.  This means that publishing an Earthbook can be done by anyone anywhere.</div>
-              </div>
+            <div class="virtue"><img src="../svgs/icon-opensource.svg" width="100%" height="100%" title="EarthBook code is fully open-source" alt="EarthBook code is fully open-source" style="cursor:pointer" onclick="openOS()"><div id="OS" class="virtue-desc">The EarthBook source code is open to review, improvement and forking <a href="https://github.com/russs95/earthbook">on Github</a>.  This means that publishing an Earthbook is open to anyone anywhere.</div>
+            </div>
 
-              <div class="virtue"><img src="../svgs/icon-biodiversity.svg"  width="100%" height="100%" title="Supporting Biodiveristy" alt="Supporting Biodiversity"><div class="virtue-desc">During the writing and publishing of this book, the species supported in the enterprise physical space was tracked and accounted for.  <a href="https://russs.net/forest" target="_blank">See the publisher's full life list.</a></div>
-              </div>
+            <div class="virtue"><img src="../svgs/icon-biodiversity.svg"  width="100%" height="100%" title="Supporting, Accountin & Tracking Biodiveristy" alt="Supporting, Accountin & Tracking Biodiveristy" style="cursor:pointer" onclick="openBio()"><div id="bio" class="virtue-desc">During the writing and publishing of this book, the species supported by the enterprise's physical space was tracked and accounted.  <a href="https://russs.net/forest" target="_blank">See the publisher's full life list.</a></div>
+            </div>
 
-              <div class="virtue"><img src="../svgs/icon-ecoaccounting.svg"  width="100%" height="100%" title="Ecological Accounting Kept" alt="Ecological Accounting Kept"><div class="virtue-desc">Throughout the enterprise of writing and publishing an Earthbook impac, the carbon, plastic and biodiversity impacts were tracked, accounted and annually disclosed.  See the full <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank">Regenerativity Report.</a></div>
-              </div>
+            <div class="virtue"><img src="../svgs/icon-ecoaccounting.svg"  width="100%" height="100%" title="Ecological Accounting Disclosure" alt="Ecological Accounting Disclosure" style="cursor:pointer" onclick="openAcc()"><div id="acc" class="virtue-desc">Throughout the enterprise of writing and publishing an Earthbook impac, the carbon, plastic and biodiversity impacts were tracked, accounted and annually disclosed.  See the full <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank">Regenerativity Report.</a></div>
+            </div>
 
             </div>
 
             <div id="virtue-box-mobile">
 
-              <div class="virtue"><img src="../svgs/icon-for-earth.svg"  width="100%" height="100%" title="For-Earth Enterprise" alt="For-earth Enterprise"><div class="virtue-desc">The net impact of the writing and publishing of the book was the subtraction of carbon.</div>
-              </div>
+            <div class="virtue"><img src="../svgs/icon-for-earth.svg"  width="100%" height="100%" title="For-Earth Enterprise" alt="For-Earth Enterprise"  onclick="openForE()"><div id="fore" class="virtue-desc">The intention and structure of this publishing enterprise is not-for-profit, for-Earth benefit.</div>
+            </div>
 
-              <div class="virtue"><img src="../svgs/icon-carbonseq.svg"  width="100%" height="100%" title="Net-Subtractive Carbon Impact" alt="Net-Subtractive Carbon Impact"><div class="virtue-desc">The Earthbook format is designed from the ground up to have a minimal carbon footprint.  <div id="wcb" class="carbonbadge wcb-d"></div></div>
-              </div>
+            <div class="virtue"><img src="../svgs/icon-carbonseq.svg"  width="100%" height="100%" title="Net-Subtractive Carbon Impact" alt="Net-Subtractive Carbon Impact"  onclick="openCo2()"><div id="co2" class="virtue-desc">Crafted from fully open and non-proprietary web protocols, an Earthbook has the lightest digital footprint compared to other eReader formats. Thanks to its byte-optimization and offline cacheability, an Earthbook's digital emissions footprint is in the top 10% of all websites.  <span style="cursor:pointer; text-decoration:underline;" onclick="openEco()">View live CO2e audit.</span></div>
+            </div>
 
-              <div class="virtue"><img src="../svgs/icon-creativecommons.svg"  width="100%" height="100%" title="Creative Commons" alt="Creative Commons License"><div class="virtue-desc">The code and content of this book are licensed under a Creative Commons BY-SA-ND 4.0 License.</div>
-              </div>
+            <div class="virtue"><img src="../svgs/icon-creativecommons.svg"  width="100%" height="100%" title="Creative Commons" alt="Creative Commons License"  onclick="openCc()"><div id="cc" class="virtue-desc">The code and content of this book are licensed under a Creative Commons BY-SA-ND 4.0 License. <a href="https://github.com/russs95/earthbook/" target="_blank"> Access the Github repository.</a></div>
+            </div>
 
           </div>
 
           <div id="virtue-box-mobile" style="margin-bottom:170px;">
 
-            <div class="virtue"><img src="../svgs/icon-readfreely.svg"  width="100%" height="100%" title="Read Freely" alt="Read Freely"><div class="virtue-desc">This book is available to read freely online and as a downloadable PDF.</div>
-            </div>
+          <div class="virtue"><img src="../svgs/icon-readfreely.svg"  width="100%" height="100%" title="Read Freely" alt="Read Freely"  onclick="openFree()"><div id="free" class="virtue-desc">This book is available to read <a href="preamble.html">freely online</a> and as a <a href="https://files.earthen.io/s/ZjS9TgWk5Tr7b4q" target="_blank">free downloadable PDF</a>.</div>
+          </div>
 
-            <div class="virtue"><img src="../svgs/icon-revenue.svg"  width="100%" height="100%" title="Support Independent Publishing" alt="Support Independent Publishing"><div class="virtue-desc">Earthbook are 100% independent of corporate platforms and enable author's to publish and sell their own work without a middle man.  97% of the proceeds of the <span style="cursor:pointer" onclick="openBuy()">sale of this book</span> go to the authors.</div>
-            </div>
+          <div class="virtue"><img src="../svgs/icon-revenue.svg"  width="100%" height="100%" title="Support Independent Publishing" alt="Support Independent Publishing" onclick="openRev()"><div id="rev" class="virtue-desc">Earthbooks are 100% independent of corporate middlemen. 97% of the proceeds of <span style="cursor:pointer; text-decoration:underline;" onclick="openBuy()">Ebook sales</span> go to the authors.</div>
+          </div>
 
-            <div class="virtue" ><img src="../svgs/icon-accessibility.svg"  width="100%" height="100%" title="Accessibility Maximized" alt="Accessibility Maximized"><div class="virtue-desc">Earthbooks are designed from the ground up to be easy on the eyes, accessible, and multi-lingual.</div>
+          <div class="virtue"><img src="../svgs/icon-accessibility.svg"  width="100%" height="100%" title="Made as human-friendly as possible" alt="Accessibility Maximized"  style="cursor:pointer" onclick="openMax()"><div id="max" class="virtue-desc">Earthbooks are designed from the ground up to be easy on the eyes, accessible, and multi-lingual.  <span style="cursor:pointer; text-decoration:underline;" onclick="openSettings()">Try adjusting the Earthbook's settings.</span></div>
+          </div>
             </div>
 
         </div>
