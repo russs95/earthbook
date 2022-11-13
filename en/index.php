5d97540e5bc0130f6d42aa184ea3f079d79f3bd9
russmaier <russmaier@gmail.com>
2022-11-13 21:33:54 +0800

    index op

--- en/index.php
+++ en/index.php
@@ -2,28 +2,127 @@
 
 Concept and first version by Russell Maier, October 24th, 2022.  First used for the book Tractus Ayyew - An Earthen Ethics (all forked generic versions of the Earthbook are requested to leave this book in as the place holder text)-->
     
+Design by Russell Maier
+
+CSS Version 0.7.3
+Original Author:  Russell Maier
+License: GNU General Public License v2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+All files, unless otherwise stated, are released under the GNU General Public
+License version 2.0 (https://www.gnu.org/licenses/gpl-2.0.html)
+See full project repository at: https://github.com/russs95/earthbook
+*/
+-->
+
+
 <!DOCTYPE html>
 <html lang="en">
-    
+
 <head>
 
 <meta charset="UTF-8"> 
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="article:modified_time" content="2022-10-31T09:14:13+00:00" />
+
+<!--BOOK META TAGS
+These tags will be consistent for the whole book-->
+
+<meta property="og:site_name" content="Tractatus Ayyew" />
+<meta property="article:publisher" content="Earthen.io" />
+<meta name="author" content="Russell Maier & Banayan Angway">
+<meta property="og:type" content="book" />
+<link rel="apple-touch-icon" sizes="180x180" href="../favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../favicons/favicon-16x16.png">
+<link rel="manifest" href="../favicons/site.webmanifest">
+
+<meta name="twitter:card" content="summary" />
+<meta property="og:type"          content="book">
+<meta property="og:image"         content="https://book.earthen.io/covers/earth-book-cover-1000px.jpg"/>
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="1000px" />
+<meta property="og:image:height" content="1500px" />
+
+<meta property="og:locale" content="en_GB" />
+
+ <!-- INCLUDES
+ Main Scripts-->   
+
+ <script src="../content-javascript.js" defer></script>
+ <script src="../universal-javascript.js" defer></script>
+ <script src="../clip/dist/clipboard.min.js" defer></script>
+ <script src="../accessibility.js" defer></script>
+ <script type="module" src="https://unpkg.com/dark-mode-toggle" async></script>
+ 
+ <!-- Arc CDN Script without delay
+<script async src="https://arc.io/widget.min.js#Z7EC7Cze"></script>-->
 
 
- <!--Page Icons-->
- <link rel="apple-touch-icon" sizes="180x180" href="../favicons/apple-touch-icon.png">
- <link rel="icon" type="image/png" sizes="32x32" href="../favicons/favicon-32x32.png">
- <link rel="icon" type="image/png" sizes="16x16" href="../favicons/favicon-16x16.png">
- <link rel="manifest" href="../favicons/site.webmanifest">
+ <!-- Style Sheets   
+All the css needed for this page-->
+ 
+ 
+ <!--<link  rel="stylesheet" type="text/css" href="../style-sheet.css?v1.3">
+<link rel="stylesheet" href="../acessibilidade.css" />
+<link rel="stylesheet" type="text/css" href="../stylesheet-chapter.css">-->
+<link rel="preload" href="../light.css?v1.4" as="style" onload="this.rel='stylesheet'">
+<link rel="preload" href="../style-sheet.css?v1.4" as="style" onload="this.rel='stylesheet'">
+<link rel="preload" href="../acessibilidade.css" as="style" onload="this.rel='stylesheet'">
+<link rel="preload" href="../stylesheet-chapter.css" as="style" onload="this.rel='stylesheet'">
 
+<link rel="stylesheet" href="../light.css" media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)">
+ <link rel="stylesheet" href="../dark.css" media="(prefers-color-scheme: dark)">
+ 
+
+ <!--Page Components
+All the page components required to build this page-->
+
+ <!--1--><script src="includes/header-component.js" type="text/javascript" preload></script>
+ <!--2--><script src="includes/settings-curtain.js" type="text/javascript" defer></script>
+ <!--3--><script src="includes/content-curtain.js" type="text/javascript" defer></script>
+ <!--4--><script src="includes/share-curtain.js" type="text/javascript" defer></script>
+ <!--5--><script src="includes/buy-curtain.js" type="text/javascript" defer></script>
+ <!--6--><script src="includes/bookplate-curtain.js" type="text/javascript" defer></script>
+ <!--7--><script src="includes/earthbook-curtain.js" type="text/javascript" defer></script>
+ <!--8b--><script src="includes/footer-chapter.js" type="text/javascript" defer></script>
+  <!--9<script src="includes/eco-curtain.js" type="text/javascript" defer></script>-->
+
+
+ <!-- Fonts
+The requisite typography for the page-->
+
+<link rel="preconnect" href="../fonts/">
+<link rel="preconnect" href="../fonts/" crossorigin> 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<!-- Mulish loads first for first page view-->
+
+<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500&display=swap" rel="stylesheet" media="print" onload="this.media='all'"> 
 
 	
-<link rel="canonical" href="https://book.earthen.io/en/"> 
-<link rel="alternate" href="https://book.earthen.io/en/" hreflang="en" />
-<link rel="alternate" href="https://book.earthen.io/en/" hreflang="x-default" />
+<!--Preload graphics-->
+<link rel="preload" as="image" href="../photos/cover-500px.webp">
 
-<!--META TAGS-->
+
+
+
+<!--END OF GENERIC CONTENT-->
+
+<!--PAGE LINKS
+
+**** Must be updated for each unique page ****-->
+
+<link rel="canonical" href="https://book.earthen.io/en"> 
+<link rel="alternate" href="https://book.earthen.io/en" hreflang="en" />
+<link rel="alternate" href="https://book.earthen.io/en" hreflang="x-default" />
+<!--<link rel="alternate" href="https://book.earthen.io/fr" hreflang="fr" />
+<link rel="alternate" href="https://book.earthen.io/es" hreflang="es" />
+<link rel="alternate" href="https://book.earthen.io/id" hreflang="id" />-->
+
+<!--META TAGS
+Must be updated for each page-->
+
 
 <title>Tractatus Ayyew | An Earthen Ethic - Earthbook</title>
 
@@ -31,78 +130,17 @@ Concept and first version by Russell Maier, October 24th, 2022.  First used for 
 
 <meta name="description" content="A Theory of Grey & Green based on Earth's example.">
 
-<meta name="author" content="Earthen.io">
-<meta property="og:type" content="book" />
-<meta property="article:modified_time" content="2022-10-24T09:14:13+00:00" />
-<meta property="og:image:type" content="image/png" />
-<meta name="author" content="Tractatus Ayyew | An Earthen Ethic - Earthbook" />
-<meta name="twitter:card" content="summary" />
+
 <meta name="twitter:label1" content="Est. reading time" />
-<meta name="twitter:data1" content="2 hours" /> 
+<meta name="twitter:data1" content="3 minutes" /> 
 	
 <!-- Facebook Open Graph Tags for social sharing-->
-<meta property="og:site_name" content="Tractatus Ayyew" />
-<meta property="article:publisher" content="Earthen.io" />
-<meta property="og:url"           content="https://book.earthen.io/en/">
-<meta property="og:type"          content="website">
-<meta property="og:title"         content="Tractatus Ayyew - An Earthen Ethics">
-<meta property="og:description"   content="By Banayan Angway & Russell Maier  | Earthbook Edition."/>
-<meta property="og:image"         content="https://book.earthen.io/photos/earth-book-cover-1000px.jpg"/>
-<meta property="og:image:width" content="1000" />
-<meta property="og:image:height" content="1500" />
-<meta property="og:image:alt"     content="The Earth and its geometric pattern floating in space"/>
-  <meta property="og:locale" content="en_GB" />
-
- <!--INCLUDES-->   
-
-<script src="../index-javascripts.js" defer></script>
-<script src="../universal-javascript.js" defer></script>
-<script src="../clipboard.min.js"></script>
-<script src="../accessibility.js"></script>
-
-<script type="module" src="https://unpkg.com/dark-mode-toggle"></script>
-
-<!--
-<script type="text/javascript">app_id="63549d9864728f002225d0dc";distribution_key="dist_2";</script><script type="text/javascript" src="https://loader.knack.com/63549d9864728f002225d0dc/dist_2/knack.js" defer></script>-->
-
-
-
-
-<!-- Style Sheets -->
-<link rel="stylesheet" type="text/css" href="../style-sheet.css?v1.4">
-<link rel="stylesheet" href="/dark.css?v=1" media="(prefers-color-scheme: dark)">
-<link rel="stylesheet" href="/light.css?v=1" media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)">
-<link rel="stylesheet" href="../acessibilidade.css" />
-
-<!-- Fonts-->
-<link rel="preconnect" href="../fonts/">
-<link rel="preconnect" href="../fonts/" crossorigin> 
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400&family=Roboto&display=swap" rel="stylesheet"> 
-
- <!--1--><script src="includes/header-component.js" type="text/javascript" defer></script>
- <!--2--><script src="includes/settings-curtain.js" type="text/javascript" defer></script>
- <!--3--><script src="includes/content-curtain.js" type="text/javascript" defer></script>
- <!--4--><script src="includes/share-curtain.js" type="text/javascript" defer></script>
- <!--5--><script src="includes/buy-curtain.js" type="text/javascript" defer></script>
- <!--6--><script src="includes/bookplate-curtain.js" type="text/javascript" defer></script>
- <!--7--><script src="includes/earthbook-curtain.js" type="text/javascript" defer></script>
- <!--8a--><script src="includes/footer-component.js" type="text/javascript" defer></script>
-<!--9<script src="includes/eco-curtain.js" type="text/javascript" defer></script>-->
-
-
-
-
-
-
-
-<!-- Arc CDN Script without delay
-<script async src="https://arc.io/widget.min.js#Z7EC7Cze"></script>-->
+<meta property="og:url"           content="https://tractatus.earthen.io/en">
+<meta property="og:title"         content="Tractatus Ayyew | An Earthen Ethic - Earthbook">
+<meta property="og:description"   content="Welcome to the Earthbook edition of the Tractatus Ayyew"/>
 
 
 <style>
-
 
 /*Dark Toggler Compromise*/
 
@@ -187,24 +225,23 @@ Concept and first version by Russell Maier, October 24th, 2022.  First used for 
     <div class="eco-header">The Earthbook format is hand-coded from the ground up to be Green.</div>
 
     <div class="eco-text">
-    In an Earthbook's writing & publishing, its carbon, plastic and biodiversity impacts are tracked, accounted and disclosed.  See this Earthbook's full <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank">Regenerativity Report.</a>
+    In an Earthbook's writing & publishing, its carbon, plastic and biodiversity impacts are tracked, accounted and disclosed.  See this Earthbook's full <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank" aria-label="The Regen Report for our book">Regenerativity Report.</a>
     </div>
 
-    <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank">
-    <div><img src="../banners/forearth-dark-350px.png" width="300px"></div></a>
+    <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank" aria-label="The Regen Report for our book">
+    <div><img src="../banners/forearth-dark-350px.png" width="300px" alt="Banner declaring our For-Earth intention"></div></a>
 
      <div id="wcb" class="carbonbadge wcb-d"></div>
     <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" preload></script>
 
     <div class="eco-medium">The Medium is the Message.</div>
-    <span style="cursor:pointer" onclick="closeEco()">
+    <span style="cursor:pointer" onclick="closeEco()" aria-label="Close">
         <div id="bottom-close-button">
-            <img src="../svgs/bottom-x.svg">
+            <img src="../svgs/bottom-x.svg" alt="Close button">
         </div>
     </span>
   </div>
 </div>
-
 
     
 
@@ -216,7 +253,7 @@ Concept and first version by Russell Maier, October 24th, 2022.  First used for 
 	<div id="row">
         <div id="side">
 
-            <img src="../photos/cover-500px.webp" alt="Tractatus Ayyew - EarthBook">
+            <img src="../photos/cover-500px.webp" alt="Tractatus Ayyew - EarthBook Cover">
 
             <div class="welcome-to">Welcome to the <b><span style="cursor:pointer; text-decoration:underline;" onclick="openEarthbook()">EarthBook</span></b> edition of the Tractatus Ayyew.</div>
 
