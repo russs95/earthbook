d7848bf047626b711f81b241fdffaa7e2682c396
russmaier <russmaier@gmail.com>
2022-11-14 10:56:34 +0800

    hh

--- universal-javascript.js
+++ universal-javascript.js
@@ -71,19 +71,7 @@ $(function() {
 
 /* COPY FUNTION ON SHARE CURTAIN */
 
-var clipboard = new ClipboardJS('.btn');
-    
-clipboard.on('success', function (e) {
-  console.info('Action:', e.action);
-  console.info('Text:', e.text);
-  console.info('Trigger:', e.trigger);
-});
 
-clipboard.on('error', function (e) {
-  console.info('Action:', e.action);
-  console.info('Text:', e.text);
-  console.info('Trigger:', e.trigger);
-});
 
 
 
