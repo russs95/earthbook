d8b19a0168bae6b433d976f704230e92cd673ad5
russmaier <russmaier@gmail.com>
2022-11-27 20:15:49 +0800

    go

--- en/includes/read-curtain.js
+++ en/includes/read-curtain.js
@@ -21,16 +21,22 @@ class ReadCurtain extends HTMLElement {
 
                   <div class="read-book-info">
                     <div class="read-book-info-title">Read. Freely.</div>
-                    <div class="read-book-info-text">The Earthbook version of the Tractatus Ayyew is made freely available both on and offline.  Click to activate offline caching so you can read both on and offline. Alternatively, you may purchase the eBook versions in EPUB or MOBI formats.  The PDF version is also freely available.</div>
+                    <div class="read-book-info-text">
+                      The Earthbook version of the Tractatus Ayyew is freely available.  Read now on the web (we'll set you up so you can read both online and off). Alternatively, you can download the PDF version or purchase the eBook versions in EPUB or MOBI formats.
+                    </div>
 
                     <div class="button-row">
+                      <div id="read-btn" class="action-btn">
 
-                        <div id="read-btn" class="action-btn">
-                        <a href="earthbook.php"><i style="background-image: url(../icons/reader.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 5px;"></i>PDF</a></div>
-
-                        <div id="buy-btn" class="action-btn" style="cursor:pointer;" onclick="openBuy()"><i style="background-image: url(../icons/buy.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 5px;"></i>eBook</a></div>
+                        <a href="preamble.html"><i style="background-image: url(../icons/reader.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 5px;"></i>Web</a>
                         
-                        <div id="pdf-btn" class="action-btn"><a href="https://files.earthen.io/s/ZjS9TgWk5Tr7b4q"><i style="background-image: url(../icons/pdf.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 2px;"></i> PDF</a></div>
+                      </div>
+
+                      <div id="buy-btn" class="action-btn" style="cursor:pointer;" onclick="openBuy()">
+                      <i style="background-image: url(../icons/buy.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 5px;"></i>eBook</a>
+                      </div>
+                        
+                      <div id="pdf-btn" class="action-btn"><a href="https://files.earthen.io/s/ZjS9TgWk5Tr7b4q"><i style="background-image: url(../icons/pdf.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 2px;"></i> PDF</a></div>
 
                     
                      </div>
@@ -41,7 +47,7 @@ class ReadCurtain extends HTMLElement {
               
 
 
-            <div id="bottom-close-button" style="cursor:pointer" onclick="closeBuy()" aria-label="Click to close this pop-up page">
+            <div id="bottom-close-button" style="cursor:pointer" onclick="closeRead()" aria-label="Click to close this pop-up page">
             <img src="../svgs/left-x.svg" alt="Buy page close button">
         </div>
       </div>
