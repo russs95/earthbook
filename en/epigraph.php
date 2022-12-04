d39a8fc798511bad7c75088a127d91b7702e0244
russmaier <russmaier@gmail.com>
2022-12-04 18:07:38 +0800

    epi up

--- en/epigraph.php
+++ en/epigraph.php
@@ -34,30 +34,6 @@ $name = "";?>
 
 <?php require_once ("../header.php");?>
 
-<meta charset="UTF-8"> 
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta property="article:modified_time" content="2022-12-03T09:14:13+00:00" />
-
-<!--BOOK META TAGS
-These tags will be consistent for the whole book-->
-
-<meta property="og:site_name" content="Tractatus Ayyew" />
-<meta property="article:publisher" content="Earthen.io" />
-<meta name="author" content="Russell Maier & Banayan Angway">
-<meta property="og:type" content="book" />
-<link rel="apple-touch-icon" sizes="180x180" href="../favicons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="../favicons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="../favicons/favicon-16x16.png">
-<link rel="manifest" href="../favicons/site.webmanifest">
-
-<meta name="twitter:card" content="summary" />
-<meta property="og:type"          content="book">
-<meta property="og:image"         content="https://book.earthen.io/covers/earth-book-cover-1000px.jpg"/>
-<meta property="og:image:type" content="image/png" />
-<meta property="og:image:width" content="1000px" />
-<meta property="og:image:height" content="1500px" />
-
-<meta property="og:locale" content="en_GB" />
 
  <!-- Style Sheets   
 All the css needed for this page-->
@@ -105,31 +81,7 @@ All the page components required to build this page-->
  <!--7--><script src="includes/earthbook-curtain.js" type="text/javascript" defer></script>
  <!--8b--><script src="includes/footer-chapter.js" type="text/javascript" defer></script>
 
-
- <!-- Fonts
-The requisite typography for the page-->
-
-<link rel="preconnect" href="../fonts/">
-<link rel="preconnect" href="../fonts/" crossorigin> 
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-<!-- Mulish loads first for first page view-->
-
-<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500&display=swap" rel="stylesheet" media="print" onload="this.media='all'"> 
-
-	
-<!--Preload graphics-->
-<link rel="preload" as="image" href="../svgs/footer-settings-button.svg">
-<link rel="preload" as="image" href="../svgs/footer-settings-button-over.svg">
-<link rel="preload" as="image" href="../svgs/footer-settings-button-up.svg">
-
-
 <!--END OF GENERIC CONTENT-->
-
-<!--PAGE LINKS
-
-**** Must be updated for each unique page ****-->
 
 <!--META TAGS
 Must be updated for each page-->
--- header.php
+++ header.php
@@ -4,3 +4,49 @@
 <!--<link rel="alternate" href="https://book.earthen.io/fr/<?php echo ($name); ;?>" hreflang="fr" />
 <link rel="alternate" href="https://book.earthen.io/es/<?php echo ($name); ;?>" hreflang="es" />
 <link rel="alternate" href="https://book.earthen.io/id/<?php echo ($name); ;?>" hreflang="id" />-->
+
+
+<meta charset="UTF-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="article:modified_time" content="2022-12-03T09:14:13+00:00" />
+
+<!--BOOK META TAGS
+These tags will be consistent for the whole book-->
+
+<meta property="og:site_name" content="Tractatus Ayyew" />
+<meta property="article:publisher" content="Earthen.io" />
+<meta name="author" content="Russell Maier & Banayan Angway">
+<meta property="og:type" content="book" />
+<link rel="apple-touch-icon" sizes="180x180" href="../favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../favicons/favicon-16x16.png">
+<link rel="manifest" href="../favicons/site.webmanifest">
+
+<meta name="twitter:card" content="summary" />
+<meta property="og:type"          content="book">
+<meta property="og:image"         content="https://book.earthen.io/covers/earth-book-cover-1000px.jpg"/>
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="1000px" />
+<meta property="og:image:height" content="1500px" />
+
+<meta property="og:locale" content="<?php echo ($lang); ;?>" />
+
+
+ <!-- Fonts
+The requisite typography for the page-->
+
+<link rel="preconnect" href="../fonts/">
+<link rel="preconnect" href="../fonts/" crossorigin> 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<!-- Mulish loads first for first page view-->
+
+<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500&display=swap" rel="stylesheet" media="print" onload="this.media='all'"> 
+
+	
+<!--Preload graphics-->
+<link rel="preload" as="image" href="../svgs/footer-settings-button.svg">
+<link rel="preload" as="image" href="../svgs/footer-settings-button-over.svg">
+<link rel="preload" as="image" href="../svgs/footer-settings-button-up.svg">
+
