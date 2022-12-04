e4e17520019e842330660a7d8113c80e38c6fb8a
russmaier <russmaier@gmail.com>
2022-12-04 17:51:24 +0800

    header

---
+++ header.php
@@ -0,0 +1,92 @@
+<head>
+
+<meta charset="UTF-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="article:modified_time" content="2022-12-03T09:14:13+00:00" />
+
+<!--BOOK META TAGS
+These tags will be consistent for the whole book-->
+
+<meta property="og:site_name" content="Tractatus Ayyew" />
+<meta property="article:publisher" content="Earthen.io" />
+<meta name="author" content="Russell Maier & Banayan Angway">
+<meta property="og:type" content="book" />
+<link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+<link rel="manifest" href="favicons/site.webmanifest">
+
+<meta name="twitter:card" content="summary" />
+<meta property="og:type"          content="book">
+<meta property="og:image"         content="https://book.earthen.io/covers/earth-book-cover-1000px.jpg"/>
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="1000px" />
+<meta property="og:image:height" content="1500px" />
+
+<meta property="og:locale" content="en_GB" />
+
+ <!-- Style Sheets   
+All the css needed for this page-->
+ 
+ 
+ <!--<link  rel="stylesheet" type="text/css" href="style-sheet.css?v1.3">
+<link rel="stylesheet" href="acessibilidade.css" />
+<link rel="stylesheet" type="text/css" href="stylesheet-chapter.css">-->
+
+<link rel="preload" href="style-sheet.css?v1.6" as="style" onload="this.rel='stylesheet'">
+<link rel="preload" href="light.css?v1.5" as="style" onload="this.rel='stylesheet'">
+<link rel="preload" href="acessibilidade.css" as="style" onload="this.rel='stylesheet'">
+<link rel="preload" href="stylesheet-chapter.css" as="style" onload="this.rel='stylesheet'">
+<link rel="preload" as="image" href="svgs/up-arrow-dark.svg">
+<link rel="preload" as="image" href="svgs/up-arrow-light.svg">
+
+<link rel="stylesheet" href="light.css" media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)">
+ <link rel="stylesheet" href="dark.css" media="(prefers-color-scheme: dark)">
+ 
+
+
+ <!-- INCLUDES
+ Main Scripts-->   
+
+ <script src="content-javascript.js" defer></script>
+ <script src="universal-javascript.js" defer></script>
+ <script src="clip/dist/clipboard.min.js"></script>
+ <script src="accessibility.js" defer></script>
+ <script type="module" src="https://unpkg.com/dark-mode-toggle" async></script>
+ 
+ <!-- Arc CDN Script without delay
+<script async src="https://arc.io/widget.min.js#Z7EC7Cze"></script>-->
+
+
+
+ <!--Page Components
+All the page components required to build this page-->
+
+ <!--1--><script src="includes/header-component.js" type="text/javascript" preload></script>
+ <!--2--><script src="includes/settings-curtain.js" type="text/javascript" defer></script>
+ <!--3--><script src="includes/content-curtain.js" type="text/javascript" defer></script>
+ <!--4--><script src="includes/share-curtain.js" type="text/javascript" defer></script>
+ <!--5--><script src="includes/buy-curtain.js" type="text/javascript" defer></script>
+ <!--6--><script src="includes/bookplate-curtain.js" type="text/javascript" defer></script>
+ <!--7--><script src="includes/earthbook-curtain.js" type="text/javascript" defer></script>
+ <!--8b--><script src="includes/footer-chapter.js" type="text/javascript" defer></script>
+
+
+ <!-- Fonts
+The requisite typography for the page-->
+
+<link rel="preconnect" href="fonts/">
+<link rel="preconnect" href="fonts/" crossorigin> 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<!-- Mulish loads first for first page view-->
+
+<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500&display=swap" rel="stylesheet" media="print" onload="this.media='all'"> 
+
+	
+<!--Preload graphics-->
+<link rel="preload" as="image" href="svgs/footer-settings-button.svg">
+<link rel="preload" as="image" href="svgs/footer-settings-button-over.svg">
+<link rel="preload" as="image" href="svgs/footer-settings-button-up.svg">
+
