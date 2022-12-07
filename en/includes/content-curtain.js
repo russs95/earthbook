a7a6443f1e542338d39749a61a08517f9dfa4a1c
russmaier <russmaier@gmail.com>
2022-12-07 11:16:09 +0800

    home button

--- en/includes/content-curtain.js
+++ en/includes/content-curtain.js
@@ -8,8 +8,8 @@ class ContentCurtain extends HTMLElement {
 
     <div id="table-of-contents" class="overlay-tc">
 
-    <div id="top-close-button" style="cursor:pointer" onclick="closeContents()">
-    <img src="../svgs/bottom-x.svg" alt="Close Button"><img src="../svgs/bottom-x.svg" alt="Close Button">
+    <div id="top-close-button">
+    <img src="../icons/home.svg" alt="Close Button"> <img src="../svgs/bottom-x.svg" alt="Close Button" style="cursor:pointer" onclick="closeContents()">
     </div>
         
         <div class="overlay-content-tc">
