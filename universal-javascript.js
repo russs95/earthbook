6d638730f14d8d2c1b2947b9b19d8e500f71fc18
Russell Maier <68637813+russs95@users.noreply.github.com>
2023-01-22 15:33:11 +0000

    Adding search

--- universal-javascript.js
+++ universal-javascript.js
@@ -483,7 +483,60 @@ containerBrightness.setAttribute("style", "filter: brightness("+val+"%); height:
 
 /* -------------------------------------------------------------------------- */
 
-/*	4. Misc
+/*	4. SEARCH
 
 /* -------------------------------------------------------------------------- */
+
+
+ function searchPosts( loadedResults ){  
+      
+    var query = document.getElementById( "search_input" ).value;  
+      
+    var resultsContainer = document.getElementById( "search_results" );  
+      
+    // clear results container if no previous results have been loaded  
+    if( loadedResults === 0 ){  
+        resultsContainer.innerHTML = "";  
+    }  
+      
+    // create XMLHttpRequest object  
+    var xmlhttp = new XMLHttpRequest();  
+      
+    // create function that is called when request is completed  
+    xmlhttp.onreadystatechange = function() {  
+        if ( xmlhttp.readyState === 4 && xmlhttp.status === 200 ) {  
+            // fetch response text   
+            var response=xmlhttp.responseText;  
+            var outputPosts;   
+               
+            // parse response if it is valid JSON  
+            try{  
+                outputPosts = JSON.parse( response );  
+            }  
+            catch( e ){  
+                return;  
+            }  
+              
+            // iterate over results  
+            for( var i = 0; i < outputPosts.length; i++ ){  
+                // append result to result container, link to url of post  
+                resultsContainer.innerHTML += "<div id='result_" + i + "'><a href='http://" + outputPosts[ i ].url + "'><h3>" + outputPosts[ i ].title + "</h3>" + outputPosts[ i ].description + "</a><div>";  
+            }  
+            // add button to load more results starting from the last loaded result (remove any existing button first if one exists)  
+            try{  
+                document.getElementById( "load_button" ).remove();  
+            }  
+            catch( e ){  
+                return;  
+            }  
+            finally{  
+                resultsContainer.innerHTML += "<br><button id='load_button' onclick='searchPosts( " + ( loadedResults + outputPosts.length ) + " )'>Load more</button>";  
+            }  
+        }  
+    };  
+      
+    // send request to fetch searchDB.php  
+    xmlhttp.open( "GET", "search.php?search=" + query + "&loaded=" + loadedResults, true );  
+    xmlhttp.send();  
+}
 
