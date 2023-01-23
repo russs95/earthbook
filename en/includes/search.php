07876df9cd010233a464e834b8f447c43548fdce
Russell Maier <68637813+russs95@users.noreply.github.com>
2023-01-23 00:35:27 +0000

    Update search.php

--- en/includes/search.php
+++ en/includes/search.php
@@ -10,13 +10,14 @@
       <div class="search-overlay-content">
             
             <div>
-                <h1 class="accessibility-plugin-ac">Search the book</h1> 
-              <h3>Search by keyword or phrase.</h3>-->
+                <h1 class="accessibility-plugin-ac">Search the Earthbook</h1> 
+              <h3>Search by keyword or phrase.</h3>
             </div>
           
             <div class="search-box">
               
               <input id="search_input" type="text" placeholder="Search...">   
+                
 <button onclick="searchPosts(0)">Search</button>   
    
 <div id="search results"></div>  
