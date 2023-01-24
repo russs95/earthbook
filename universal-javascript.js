85d23909a096211f3a2d1fe2e0c960a2a7523b1e
Russell Maier <russ@earthen.io>
2023-01-24 16:24:20 -0500

    jav

--- universal-javascript.js
+++ universal-javascript.js
@@ -329,11 +329,12 @@ function searchPosts( loadedResults ){
                 return;  
             }  
               
-            // iterate over results  
-            for( var i = 0; i < outputPosts.length; i++ ){  
+               // iterate over results  
+               for( var i = 0; i < outputPosts.length; i++ ){  
                 // append result to result container, link to url of post  
-                resultsContainer.innerHTML += "<div id='result_" + i + "'><a href='https://" + outputPosts[ i ].url + "'><h3>" + outputPosts[ i ].title + "</h3>" + outputPosts[ i ].description + "</a><div>";  
+                resultsContainer.innerHTML += "<div class=\"tc-item\"><div id='result_" + i + "'><div class=\"chapter-name-tc\"><b><a href='" + outputPosts[ i ].url + "'>" + outputPosts[ i ].title + "</b>:  " + outputPosts[ i ].description + "<br>Chapter " + outputPosts[ i ].chapter + "  | Book " + outputPosts[ i ].book + "  |  Words:" + outputPosts[ i ].words + "</div>";  
             }  
+
 
             
             // add button to load more results starting from the last loaded result (remove any existing button first if one exists)  
