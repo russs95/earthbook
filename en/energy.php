1445035c45bffd4b52c59db57821b92fdfe2cec6
Russell Maier <russ@earthen.io>
2023-01-30 10:35:51 -0500

    tweak and comments

--- en/energy.php
+++ en/energy.php
@@ -324,23 +324,24 @@ Must be updated for each page-->
     <div id="footer-functions-box" style="width:100%; padding-top: 5px;">
       
       <div class="footer-function" style="cursor:pointer;" onclick="openSearch()">
-        <div><p style="font-variant-caps: all-petite-caps; color:grey; font-size: 1.1em; padding-left:7%;">+ Search EarthBook</p></div>
+        <div><p style="font-variant-caps: all-petite-caps; color:grey; font-size: 1.1em; padding-left:7%; width: 100%;">+ Search EarthBook</p></div>
         <div style="margin: auto 0px auto auto;padding-right: 7%;"><i style="background-image: url(../icons/search.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 2px;"></i></div>
       </div>
 
       <div class="footer-function" style="cursor:pointer;" onclick="openComments()">
-        <div><p style="font-variant-caps: all-petite-caps; color:grey; font-size: 1.1em; padding-left: 7%;">+ Chapter Reader Comments</p></div>
+        <div><p style="font-variant-caps: all-petite-caps; color:grey; font-size: 1.1em; padding-left: 7%; width: 100%;">+ Chapter Reader Comments</p></div>
         <div style="margin: auto 0px auto auto;padding-right: 7%;"><i style="background-image: url(../icons/comments.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 2px;"></i></div>
       </div>
 
       <div class="footer-function" style="cursor:pointer;" onclick="openComments()">
-        <div><p style="font-variant-caps: all-petite-caps; color:grey; font-size: 1.1em; padding-left: 7%;">+ Chapter Source Code</p></div>
+        <div><p style="font-variant-caps: all-petite-caps; color:grey; font-size: 1.1em; padding-left: 7%; width: 100%;">+ Chapter Source Code</p></div>
         <div style="margin: auto 0px auto auto;padding-right: 7%;"><i style="background-image: url(../icons/code.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 2px;"></i></div>
       </div>
 
       <div class="footer-function" style="cursor:pointer;" onclick="openFooter()">
-        <div><p style="font-variant-caps: all-petite-caps; color:grey; font-size: 1.1em">EarthBook resources</p></div>
-        <div style="margin: auto 0px auto auto;padding-right: 7%;padding-left: 7%;"><i style="background-image: url(../icons/code.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 2px;"></i></div>
+      <div id="footer-icon-left" style="cursor:pointer;" onclick="openFooter()"></div>
+      <div><p style="font-variant-caps: all-petite-caps; color:grey; font-size: 1.1em; padding-left: 7%; width: 100%;" >EarthBook resources</p></div>
+        <div style="margin: auto 0px auto auto;padding-right: 7%;padding-left: 7%;">+</div>
       </div>
 
     </div>
