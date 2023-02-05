6cdbeebce4404dccc695c8af76311d807ff04c6e
Russell Maier <russ@earthen.io>
2023-02-05 18:22:04 -0500

    %

--- content-javascript.js
+++ content-javascript.js
@@ -64,14 +64,14 @@ function openCode() {
   var x = document.getElementById("footer-function-reveal-code");
  // var y = document.getElementById("footer-icon-left");
   if (x.style.height === "0%") {
-    x.style.opacity = "1";
+   // x.style.opacity = "1";
     x.style.height ="100%"
    // y.style.background = "url(../svgs/footer-settings-button-up.svg) no-repeat left";
    // y.style.backgroundSize = "contain";
  
   
   } else {
-    x.style.opacity = "0";
+   // x.style.opacity = "0";
     x.style.height ="0%"
    // y.style.background = "url(../svgs/footer-settings-button.svg) no-repeat left";
    // y.style.backgroundSize = "contain";
