9ad8be3c8fcece0882af6e78238e378a217c370e
Russell Maier <russ@earthen.io>
2023-02-07 07:11:17 -0500

    comments embded

--- en/energy.php
+++ en/energy.php
@@ -127,7 +127,30 @@ Must be updated for each page-->
 	
  <?php require_once ("includes/search.php");?>
 
- <?php require_once ("includes/comments.php");?>
+ <!--
+ <?php require_once ("includes/comments.php");?>-->
+
+
+<div id="bottom-comments-overlay" class="comments-overlay">
+
+<div id="right-close-button">
+    <span style="cursor:pointer" onclick="closeComments()"><img src="../svgs/right-x.svg" alt="Close page button"></span>
+</div>
+  <div class="comments-overlay-content" >
+  <h6>The Salmon's Spin | Chapter 9</h6>
+        
+        <?php
+$cmtx_identifier = '1';
+$cmtx_reference  = 'Energy';
+$cmtx_folder     = '/comments/';
+require($_SERVER['DOCUMENT_ROOT'] . $cmtx_folder . '/frontend/index.php');
+?>
+
+
+
+</div>
+</div>  
+ 
 
 <!-- Page Title Section-->
 
