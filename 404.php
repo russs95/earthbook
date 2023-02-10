a57c73d8dfdef05094ec23f2ecd87999bc61014d
Russell Maier <russ@earthen.io>
2023-02-10 12:52:08 +0800

    sdf

---
+++ 404.php
@@ -0,0 +1,210 @@
+<!--EARTHBOOK CHAPTER PAGE TEMPLATE
+
+PHP Page Version 1.0.2
+Design by Russell Maier
+License: GNU General Public License v2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+All files, unless otherwise stated, are released under the GNU General Public
+License version 2.0 (https://www.gnu.org/licenses/gpl-2.0.html)
+See full project repository at: https://github.com/russs95/earthbook
+*/
+-->
+
+
+<!-- PHP starts by laying out canonical URLs for the page and language -->
+
+<!DOCTYPE html>
+
+<meta charset="UTF-8"> 
+
+<!-- this grabs the language identifier for the page so that it can used in the meta and canonical url variables.  It also grabs the page name.-->
+<?php require_once ("en/lang.php");
+
+echo <<<_END
+<html lang="$lang" manifest="../offline.appcache">
+_END;
+
+$parts = explode ("/", $_SERVER['SCRIPT_NAME']);
+$name = $parts [count($parts)-1];
+if (strcmp($name, "welcome.php") == 0)
+$name = "";?>
+
+<head>
+
+
+<?php require_once ("../header.php");?>
+
+
+
+<!--END OF GENERIC CONTENT-->
+
+<!--META TAGS
+Must be updated for each page-->
+
+<title>404 Page not found | Tratatus Ayyew - Earthbook</title>
+<meta name="keywords" content="not yet published, epigraph, introduction, stories, dapay, Earth ethics, earthen ethics, earth, ethics, tractatus ayyew, igorot, indigenous philosophy, philosophy, Earth, green ethics, environmental ethics, deep ecology, ecological, ethics"> 
+<meta name="description" content="This chapter hasn't yet been published.">
+
+
+<meta name="twitter:label1" content="Est. reading time" />
+<meta name="twitter:data1" content="1 minutes" /> 
+	
+<!-- Facebook Open Graph Tags for social sharing-->
+<meta property="og:title"         content="Not yet published | Tratatus Ayyew - Earthbook.">
+<meta property="og:description"   content="This chapter hasn't yet been published."/>
+
+
+
+<style>
+  
+
+@media screen and (max-width: 699px) {
+    .ct-chapter-quote {
+      width: 87%;
+      font-size: 1em;
+    }
+    .ct-quote-source {
+      width: 80%;
+      font-size: 0.9em;
+    }
+
+    #ct-chapt-graphic {
+      height: 40vh;
+      margin-top: 8vh;
+    }
+
+  }
+
+  @media screen and (min-width: 700px) and (max-width: 1324px) {
+    .ct-chapter-quote {
+      width: 70%;
+      font-size: 1.7em;
+    }
+    .ct-quote-source {
+      width: 70%;
+      font-size: 1.3em;
+    }
+  }
+    
+  @media screen and (min-width: 1325px) {
+    .ct-chapter-quote {
+    width: 50%;
+    font-size: 1.7em;
+    }
+    .ct-quote-source {
+      width: 50%;
+      font-size: 1.3em;
+    }
+  }
+
+  #ct-main .action-btn {
+    margin: -35px auto 0px auto !important;
+  }
+
+</style>
+
+
+</head>
+
+
+<!--Main HTML Begins-->
+
+<BODY class="accessibility-plugin-ac">
+  <div id="underlayer">
+
+    <a name="top"></a>
+
+<!--HEADER NAVBAR-->
+<header-component></header-component>
+
+<div id="ct-chapter-top">
+    <div id="ct-tc-menu" onclick="openContents()"></div>
+    <div class="sero" style="cursor:pointer;" onclick="openContents()">
+      <div id="ct-chapter-title">Page not found</div>   
+      <div id="ct-book-title">EarthBook Error <!--#echo var="REDIRECT_STATUS" --></div>
+      <div id="ct-chap-location">Tractatus Ayyew</div>
+      <div id="ct-word-count"><i>An Earthen Ethics</i></div>
+    </div>
+</div>
+
+ 
+<?php require_once ("en/includes/eco-curtain.php");?>
+ 
+<!-- Page Title Section-->
+
+
+<div id="ct-chapt-graphic" class="background-000">
+    <div class="ct-chapter-quote">
+    The requested page (<!--#echo var="REQUEST_URI" -->) couldn't be matched with any other in the EarthBook.  Check the URL for errors or mispellings.
+    </div>
+</div>
+
+<div id="up-arrow"></div>
+
+<div id="ct-main">
+
+  <div>
+                <!--<h1></h1>-->
+    <h3 class="accessibility-plugin-ac">You can try searchin the EarthBook to find what you're looking for...</h3> 
+  </div>
+          
+  <div class="search-box" style="padding-bottom:15%;">
+    <div class="copy-section">
+      <input id="search_input" type="text" placeholder="Search...">   
+         <button class="btn" style="padding:22px;" onclick="searchPosts(0)"><i style="background-image: url(../icons/search-white.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 2px;"></i></button> 
+      </div>  
+   
+      <div id="search_results"></div>
+      <br><br><br><br>
+     
+<div class="ct-quote-source" style="width:100% !important;">
+    Book Two and Three of the <i>Tractatus Ayyew</i> is not yet published. Subscribe to Earthen to be notified of new chapter releases.
+
+    <br><br>
+
+    <?php require_once ("../manage.php");?>
+
+    <br>
+</div>
+
+
+
+
+          
+</div><!--Closes main content block-->
+
+    
+  
+    <!--Footer Next Chapter Section: Requires customization-->
+
+<div id="footer-chapter" style="margin-bottom: 0px;">
+
+  <div class="footer-size">
+    
+    <div class="footer-left">
+
+      <div id="footer-icon-left" style="cursor:pointer;" onclick="openFooter()"></div>
+    
+     
+      <div class="next-section">
+          <div class="next-sec">Back to the start:</div>
+          <div class="sec-name"><i>Introduction</i></div>
+      </div>
+
+    </div>
+    
+
+    <div class="footer-right">
+   
+      <a href="en/preamble.php"><div class="next-button">Preamble ➔</div></a>
+
+    </div>
+  </div>
+</div>
+
+<?php require_once ("en/includes/chap-footer.php");?>
+
+
+</body>
+</html>
+
