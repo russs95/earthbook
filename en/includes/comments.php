af2d5a1eedfaadba3616160a425b0d52d72aeff5
Russell Maier <russ@earthen.io>
2023-02-14 11:19:20 +0800

    sdf

--- en/includes/comments.php
+++ en/includes/comments.php
@@ -1,28 +1,18 @@
- 
-
 
 <div id="bottom-comments-overlay" class="comments-overlay">
 
-    <div id="right-close-button">
-        <span style="cursor:pointer" onclick="closeComments()"><img src="../svgs/right-x.svg" alt="Close page button"></span>
-    </div>
-      <div class="comments-overlay-content" >
-     <!-- <h6>The Salmon's Spin | Chapter 9</h6>-->
-            
-            <?php
+<div id="right-close-button">
+    <span style="cursor:pointer" onclick="closeComments()"><img src="../svgs/right-x.svg" alt="Close page button"></span>
+</div>
+  <div class="comments-overlay-content" >
+  <h6><?php echo $page_title; ?></h6>
+        
+        <?php
 $cmtx_identifier = '1';
 $cmtx_reference  = 'Energy';
 $cmtx_folder     = '/comments/';
 require($_SERVER['DOCUMENT_ROOT'] . $cmtx_folder . '/frontend/index.php');
 ?>
 
-<div id="bottom-close-button" style="cursor:pointer" onclick="closeBookplate()" aria-label="Close this pop-up page">
-            <img src="../svgs/left-x.svg" alt="Bookplate close button">
-        </div>
-
- 
-  </div>
+</div>
 </div>  
-
-
-
