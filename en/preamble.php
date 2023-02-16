3f7a4892176ec554e37808d8530899947768f942
Russell Maier <russ@earthen.io>
2023-02-16 15:46:28 +0800

    sumaries

--- en/preamble.php
+++ en/preamble.php
@@ -107,7 +107,7 @@ Must be updated for each page-->
 
 
 <div class="page-paragraph">
-    <p>To start, Banayan and I will share the story of the pollution, short-falls and questions that sparked this book's unlikely beginning. For the reader eager for an overview, we provide a short sentence, a long sentence and a paragraph that each sum up the theory of Earthen ethics that we develop in this book— see our <a href="summaries.php">Summaries</a> section.</p>
+    <p>To start, Banayan and I will share the story of the pollution, short-falls and questions that sparked this book's unlikely beginning. For the reader eager for an overview, we provide a short sentence, a long sentence and a paragraph that each sum up the theory of Earthen ethics that we develop in this book— see our <a href="summaries.php">summaries section</a>.</p>
 
     <p>Of course, <i>how</i> a story is told (or a theory for that matter) is just as important as the sum of its sentences.  In the dap'ay, both tale and telling were inseparably intertwined.  Stories were told after a dance or a feast with the listeners gathered around a fire; rice wine in hand.<p>
 
