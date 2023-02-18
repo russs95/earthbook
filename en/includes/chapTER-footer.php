0cc8f3db8b1c2c90fe41ea5083f2b82fbdee0e41
Russell Maier <russ@earthen.io>
2023-02-18 18:34:25 +0800

    some fixes

--- en/includes/chapTER-footer.php
+++ en/includes/chapTER-footer.php
@@ -2,66 +2,58 @@
 
 
 
-<div class="footer-function" >
-        <div class="footer-function-main" onclick="openCode()" style="z-index:8;">
+  <div class="footer-function" >
+    <div class="footer-function-main" onclick="openCode()" style="z-index:8;">
             
-            <div id="code-down-arrow" style="width: 18px; height: 18px;background: url(../icons/up-arrow.svg) no-repeat center;background-size:contain;margin: auto 15px auto 0;">
-            </div>
+      <div id="code-down-arrow" style="width: 18px; height: 18px;background: url(../icons/up-arrow.svg) no-repeat center;background-size:contain;margin: auto 15px auto 0;">
+      </div>
 
-            <div class="footer-function-item">
+      <div class="footer-function-item">
               <p>Source Code</p>
-            </div>
+      </div>
 
-            <div class="footer-function-item-icon"> 
+      <div class="footer-function-item-icon"> 
                 <i style="background-image: url(../icons/code.svg);"></i>
-            </div>
-
-        </div>
-
-        <div id="footer-function-reveal-code" style="height:0px; opacity:0;">
-            <div class="github-source2" style="font-size:0.8em; font-family:Mulish;color:grey;overflow: clip;">Earthbook's are open source. Contribute, correct or fork this Earthbook's code on Github: <a href="https://github.com/russs95/earthbook/blob/main/<?php echo ($lang); ;?>/<?php echo ($name); ;?> " target="_blank" aria-label="Contribute to the code. Go to the Earthbook Github repository for this page:"><br> ➔ github.com/russs95/ earthbook/blob/main/<?php echo ($lang); ;?>/<?php echo ($name); ;?></a>
-            </div>
-         </div>
       </div>
-
-      <div class="footer-function" >
-        <div class="footer-function-main" onclick="openComments()" style="z-index:9;">
-            
-            <div id="comments-out" style="width: 18px; height: 18px;background: url(../icons/plus.svg) no-repeat center;background-size:contain;margin: auto 15px auto 0;">
-            </div>
-
-            <div class="footer-function-item">
-              <p>Comments</p>
-            </div>
-
-            <div class="footer-function-item-icon"> 
-                <i style="background-image: url(../icons/comments.svg);"></i>
-            </div>
-
-        </div> 
-      </div>
-
-
-      
-      <div class="footer-function">
-        <div class="footer-function-main" style="z-index:10"; onclick="openFooter2()">
-        
-          <div id="footer-arrow" style="width:18px;height:18px;background:url(../icons/down-arrow.svg) no-repeat center; background-size:contain;margin: auto 10px auto 0;"></div>
-
-            <div class="footer-function-item"><p>Earthbook Resources</p></div>
-            <div class="footer-function-item-icon">
-            <i style="background-image: url(../icons/settings.svg);"></i>
-            </div>
-          </div>
-        </div>
-        </div>
-
-
-      
 
     </div>
 
+    <div id="footer-function-reveal-code" style="height:0px; opacity:0;">
+            
+      <div class="github-source2" style="font-size:0.8em; font-family:Mulish;color:grey;overflow: clip;">Earthbook's are open source. Contribute, correct or fork this Earthbook's code on Github: <a href="https://github.com/russs95/earthbook/blob/main/<?php echo ($lang); ;?>/<?php echo ($name); ;?> " target="_blank" aria-label="Contribute to the code. Go to the Earthbook Github repository for this page:"><br> ➔ github.com/russs95/ earthbook/blob/main/<?php echo ($lang); ;?>/<?php echo ($name); ;?></a></div>
+    
+    </div>
+    
   </div>
+
+  <div class="footer-function" >
+    <div class="footer-function-main" onclick="openComments()" style="z-index:9;">
+          
+      <div id="comments-out" style="width: 18px; height: 18px;background: url(../icons/plus.svg) no-repeat center;background-size:contain;margin: auto 15px auto 0;">
+      </div>
+
+      <div class="footer-function-item"><p>Comments</p></div>
+
+      <div class="footer-function-item-icon"> 
+          <i style="background-image: url(../icons/comments.svg);"></i>
+      </div>
+
+    </div> 
+  </div>
+   
+  <div class="footer-function">
+    <div class="footer-function-main" style="z-index:10;" onclick="openFooter2()">
+      
+      <div id="footer-arrow" style="width:18px;height:18px;background:url(../icons/down-arrow.svg) no-repeat center; background-size:contain;margin: auto 10px auto 0;"></div>
+
+      <div class="footer-function-item"><p>Earthbook Resources</p></div>
+      <div class="footer-function-item-icon">
+          <i style="background-image: url(../icons/settings.svg);"></i>
+      </div>
+    </div>
+    
+  </div>
+</div>
   
 <div id="footer-settings">
 
