757989fc5c360f012bd4a61150c27f74fabc936e
Russell Maier <russ@earthen.io>
2023-02-18 19:24:42 +0800

    hover

--- en/includes/comments.php
+++ en/includes/comments.php
@@ -2,7 +2,7 @@
 <div id="bottom-comments-overlay" class="comments-overlay">
 
 <button type=button id="right-close-button2" aria-label="close">
-    <span style="cursor:pointer" onclick="closeComments()"><img src="../svgs/right-x.svg" alt="Close page button"></span>
+    <span style="cursor:pointer" onclick="closeComments()"><img src="../svgs/right-x.svg" alt="Close Module"></span>
 </button>
   <div class="comments-overlay-content" >
   <h6><?php echo $page_title; ?></h6>
