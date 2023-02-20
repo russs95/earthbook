aaa9dcd5e53ace90da7533a7c3c2c306b54767d1
Russell Maier <russ@earthen.io>
2023-02-20 11:07:26 +0800

    improve search and footer

--- en/includes/search.php
+++ en/includes/search.php
@@ -7,15 +7,16 @@
             
             <div>
                 <!--<h1>🔎</h1>-->
-                <h1 class="accessibility-plugin-ac">Search the Earthbook...</h1> 
+                <h1 class="accessibility-plugin-ac">Earthbook Search</h1> 
             </div>
           
             <div class="search-box" style="padding-bottom:15%;">
               <div class="copy-section">
-              
+              <label for="search_input">Search the chapters</label>
+              <input placeholder="Enter search term" aria-label="Enter search term">
               <input id="search_input" type="text" placeholder="Search...">   
                 
-              <button class="btn" style="padding:22px 0px 22px 0px;" onclick="searchPosts(0)"><i style="background-image: url(../icons/search-white.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 2px;"></i></button> 
+              <button class="btn" style="padding:22px 0px 22px 0px;" onclick="searchPosts(0)" aria-label="Search Button"><i style="background-image: url(../icons/search-white.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 2px;"></i></button> 
             </div>  
    
             <div id="search_results"></div>  
