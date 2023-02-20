32cce350043375fb0c60eaa6f940c0a9648dadc6
Russell Maier <russ@earthen.io>
2023-02-20 21:07:43 +0800

    epigraph

--- en/epigraph.php
+++ en/epigraph.php
@@ -1,7 +1,11 @@
-<!--EARTHBOOK CHAPTER PAGE TEMPLATE
+<!DOCTYPE html>
 
-PHP Page Version 1.0.2
-Design by Russell Maier
+<!--EARTHBOOK - An open source, Earth & Human Friendly Book format
+Read the book.  Improve, translate or comment on the content.  Or fork the code and publish your own.  
+
+Chapter Template Version 1.1
+Created by Russell Maier for the Tractatus Ayyew: Earthen Ethics.
+
 License: GNU General Public License v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 All files, unless otherwise stated, are released under the GNU General Public
@@ -11,46 +15,44 @@ See full project repository at: https://github.com/russs95/earthbook
 -->
 
 
-<!-- PHP starts by laying out canonical URLs for the page and language -->
+<!-- this grabs the language identifier for the page so that it can used in the meta and canonical url variables.  It also grabs the page name.  manifest="../offline.appcache-->
 
-<!DOCTYPE html>
-
-<meta charset="UTF-8"> 
-
-<!-- this grabs the language identifier for the page so that it can used in the meta and canonical url variables.  It also grabs the page name.-->
 <?php require_once ("lang.php");
 
 echo <<<_END
-<html lang="$lang" manifest="../offline.appcache">
-_END;
+<html lang="$lang">
+_END;?>
 
+<!-- this sets PHP variables for the pages that will be used later on-->
+
+<?php 
 $parts = explode ("/", $_SERVER['SCRIPT_NAME']);
 $name = $parts [count($parts)-1];
-if (strcmp($name, "welcome.php") == 0)
-$name = "";?>
+if (strcmp($name, "index.php") == 0)
+$name = "";
+$page_number = "3";
+$page_title = "Epigraph | Tractatus Ayyew - An Earthbook";?>
 
 <head>
-
 
 <?php require_once ("../header.php");?>
 
 
 
-<!--END OF GENERIC CONTENT-->
-
 <!--META TAGS
 Must be updated for each page-->
 
-<title>Epigraph | Tratatus Ayyew - Earthbook</title>
+<meta property="article:modified_time" content="2023-02-20T09:14:13+00:00">
+
 <meta name="keywords" content="epigraph, introduction, stories, nature, history, dapay, Earth ethics, earthen ethics, earth, ethics, tractatus ayyew, igorot, indigenous philosophy, philosophy, Earth,green, green ethics, environmental ethics, deep ecology, ecological, ethics, Régis Debray, Le siècle vert, Un changement de civilisation, The Green Age, A Change of Civilizations"> 
 <meta name="description" content="Another world is being born right in front of our eyes:  a whole new spirit in the way we think, hope and fear...">
 
 
-<meta name="twitter:label1" content="Est. reading time" />
-<meta name="twitter:data1" content="3 minutes" /> 
+<meta name="twitter:label1" content="Est. reading time">
+<meta name="twitter:data1" content="1 minutes"> 
 	
 <!-- Facebook Open Graph Tags for social sharing-->
-<meta property="og:title"         content="Epigraph | Tratatus Ayyew - Earthbook.">
+
 <meta property="og:description"   content="Another world is being born right in front of our eyes:  a whole new spirit in the way we think, hope and fear..."/>
 
 
@@ -108,26 +110,20 @@ Must be updated for each page-->
 <BODY class="accessibility-plugin-ac">
   <div id="underlayer">
 
-    <a name="top"></a>
+  <div id="top"></div>
 
 <!--HEADER NAVBAR-->
 <header-component></header-component>
 
 <div id="ct-chapter-top">
-    <div id="ct-tc-menu" onclick="openContents()"></div>
-    <div class="sero" style="cursor:pointer;" onclick="openContents()">
+  <button type=button id="ct-tc-menu" onclick="openContents()" aria-label="Open Table of Contents"></button>
+  <div class="ct-holder" style="cursor:pointer;" onclick="openContents()">
       <div id="ct-chapter-title">Epigraph</div>   
       <div id="ct-book-title">Tractatus Ayyew</div>
       <div id="ct-chap-location">Book One</div>
       <div id="ct-word-count"><i>67 words</i></div>
     </div>
 </div>
-
-
- 
-<?php require_once ("includes/eco-curtain.php");?>
- 
-
 
 
 <!-- Page Title Section-->
@@ -150,22 +146,31 @@ Must be updated for each page-->
     <br><br>
 
           
-    </div><!--Closes main content block-->
-  
-    <!--Footer Next Chapter Section: Requires customization-->
+    <div id="footnotes-concealer"></div>
 
+</div><!-- Closes footnote section-->
+
+</div><!--Closes main content block-->
+
+<?php require_once ("includes/eco-curtain.php");?>
+
+<?php require_once ("includes/comments.php");?>
+
+<?php require_once ("includes/search.php");?>
+
+<!--Footer Next Chapter Section: Requires customization-->
 <div id="footer-chapter" style="margin-bottom: 0px;">
 
-  <div class="footer-size">
-    
-    <div class="footer-left">
+<div class="footer-size">
 
-      <div id="footer-icon-left" style="cursor:pointer;" onclick="openFooter()"></div>
-    
-     
-      <div class="next-section">
+  <div class="footer-left">
+
+   <!-- <div id="footer-icon-left" style="cursor:pointer;" onclick="openFooter()"></div>-->
+
+ 
+    <div class="next-section">
           <div class="next-sec">Next:</div>
-          <div class="sec-name"><i>Chapter 1 | Imagine</i></div>
+          <div class="sec-name"><i>1 | Imagine</i></div>
       </div>
 
     </div>
