30fc1cf6874c6c4e7748f6281089b2e06fb4f56a
Russell Maier <russ@earthen.io>
2023-02-21 10:07:22 +0800

    fix first title

--- en/includes/header-component.js
+++ en/includes/header-component.js
@@ -17,9 +17,9 @@ class Header extends HTMLElement {
         
         
         <div id="book-title" style="background-size: 60%;">
-            <div id="main-title" onclick="openContents()" style="padding: 40px 10px 18px; font-size: 3.2em;" aria-label="Open EarthBook Contents">Tractatus Ayyew</div>
-            <div id="sub-title" onclick="openContents()"><i>An Earthen Ethics</i></div>
-            <div id="sub-earthbook" onclick="openEarthbook()" aria-label="Learn what is an Earthbook">earthBook edition</div>
+            <div id="main-title" style="padding: 40px 10px 18px; font-size: 3.2em;" >Tractatus Ayyew</div>
+            <div id="sub-title"><i>An Earthen Ethics</i></div>
+            <div id="sub-earthbook">earthbook edition</div>
         </div>
 
         <div id="share-menu">
