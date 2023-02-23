7b9667d03030930fa6dfc271a584aead010892a2
Russell Maier <russ@earthen.io>
2023-02-23 10:44:48 +0800

    back

--- header2.php
+++ header2.php
@@ -1,38 +1,39 @@
 <link rel="canonical" href="https://book.earthen.io/<?php echo ($lang); ;?>/<?php echo ($name); ;?>"> 
-<link rel="alternate" href="https://book.earthen.io/en/<?php echo ($name); ;?>" hreflang="en">
-<link rel="alternate" href="http://book.earthen.io/en/<?php echo ($name); ;?>" hreflang="x-default">
-<!--<link rel="alternate" href="https://book.earthen.io/fr/<?php echo ($name); ;?>" hreflang="fr">
-<link rel="alternate" href="https://book.earthen.io/es/<?php echo ($name); ;?>" hreflang="es">
-<link rel="alternate" href="https://book.earthen.io/id/<?php echo ($name); ;?>" hreflang="id">-->
+<link rel="alternate" href="https://book.earthen.io/en/<?php echo ($name); ;?>" hreflang="en" />
+<link rel="alternate" href="http://book.earthen.io/en/<?php echo ($name); ;?>" hreflang="x-default" />
+<!--<link rel="alternate" href="https://book.earthen.io/fr/<?php echo ($name); ;?>" hreflang="fr" />
+<link rel="alternate" href="https://book.earthen.io/es/<?php echo ($name); ;?>" hreflang="es" />
+<link rel="alternate" href="https://book.earthen.io/id/<?php echo ($name); ;?>" hreflang="id" />-->
 
 
+<meta charset="UTF-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="article:modified_time" content="2022-12-03T09:14:13+00:00" />
 
 <!--BOOK META TAGS
 These tags will be consistent for the whole book-->
-<title><?php echo $page_title; ?></title>
 
-
-<meta property="og:site_name" content="Tractatus Ayyew">
-<meta property="article:publisher" content="Earthen.io">
+<meta property="og:site_name" content="Tractatus Ayyew" />
+<meta property="article:publisher" content="Earthen.io" />
 <meta name="author" content="Russell Maier & Banayan Angway">
-
-<meta property="og:type" content="book">
+<meta property="og:type" content="book" />
 <link rel="apple-touch-icon" sizes="180x180" href="../favicons/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="../favicons/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="../favicons/favicon-16x16.png">
 <link rel="manifest" href="../favicons/site.webmanifest">
 
-<meta name="twitter:card" content="summary">
+<meta name="twitter:card" content="summary" />
 <meta property="og:type"          content="book">
-<meta property="og:image"         content="https://book.earthen.io/covers/earth-book-cover-1000px.jpg">
-<meta property="og:image:type" content="image/png">
-<meta property="og:image:width" content="1000px">
-<meta property="og:image:height" content="1500px">
+<meta property="og:image"         content="https://book.earthen.io/covers/earth-book-cover-1000px.jpg"/>
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="1000px" />
+<meta property="og:image:height" content="1500px" />
 
-<meta property="og:locale" content="<?php echo ($lang); ;?>">
+<meta property="og:locale" content="<?php echo ($lang); ;?>" />
 
-<meta property="og:url"           content="https://book.earthen.io/<?php echo $lang; ?>/<?php echo $name; ?>">
-<meta property="og:title"         content="<?php echo $page_title; ?>">
+<meta property="og:url" content="https://tractatus.earthen.io/<?php echo ($lang); ;?>/<?php echo ($name); ;?>">
+
+
  <!-- Fonts
 The requisite typography for the page-->
 
@@ -46,10 +47,10 @@ The requisite typography for the page-->
 <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500&display=swap" rel="stylesheet" media="print" onload="this.media='all'"> 
 
 	
-<!--Preload graphics
+<!--Preload graphics-->
 <link rel="preload" as="image" href="../svgs/footer-settings-button.svg">
 <link rel="preload" as="image" href="../svgs/footer-settings-button-over.svg">
-<link rel="preload" as="image" href="../svgs/footer-settings-button-up.svg">-->
+<link rel="preload" as="image" href="../svgs/footer-settings-button-up.svg">
 
  <!-- Style Sheets   
 All the css needed for this page-->
