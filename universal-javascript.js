b7565ed9a976bf26040f6cf98968269a3c64b15e
Russell Maier <russ@earthen.io>
2023-02-24 16:57:42 +0800

    agaibn

--- universal-javascript.js
+++ universal-javascript.js
@@ -84,6 +84,115 @@ $(function() {
 
 
 
+/* LEFT SETTINGS OVERLAY */
+function openSettings() {
+  document.getElementById("left-settings-overlay").style.width = "100%";
+  document.body.style.overflowY = "hidden";
+  document.body.style.maxHeight = "101vh";
+
+}
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeSettings() {
+  document.getElementById("left-settings-overlay").style.width = "0%";
+  document.body.style.overflowY = "unset";
+  document.body.style.maxHeight = "unset";
+  document.body.style.height = "unset";} 
+
+ 
+/* RIGHT SHARE OVERLAY 
+
+Triggers the right share link panel*/
+
+function openShare() {
+    document.getElementById("right-share-overlay").style.width = "100%";
+    document.body.style.overflowY = "hidden";
+    //document.body.style.maxHeight = "101vh";
+ 
+  }
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeShare() {
+    document.getElementById("right-share-overlay").style.width = "0%";
+    document.body.style.overflowY = "unset";
+    //document.body.style.maxHeight = "unset";
+
+  } 
+
+
+
+/* BOOKPLATE OVERLAY
+
+Opens the bookplate overlay from the top up*/
+
+function openBookplate() {
+  document.getElementById("top-bookplate").style.height = "100%";
+  document.body.style.overflowY = "hidden";
+ // document.body.style.maxHeight = "101vh";
+  document.getElementById("sub-earthbook").style.display = "block!important";
+  document.getElementById("bookplate-img").style.display = "block";
+}
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeBookplate() {
+  document.getElementById("top-bookplate").style.height = "0%";
+  document.body.style.overflowY = "unset";
+  //document.body.style.maxHeight = "unset";
+  document.getElementById("sub-earthbook").style.display = "none";
+  document.getElementById("bookplate-button").style.display = "none";
+  document.getElementById("bookplate-img").style.display = "none";
+
+} 
+
+
+  
+
+
+
+
+
+/* RIGHT SEARCH OVERLAY 
+
+Triggers the right search panel*/
+
+function openSearch() {
+  document.getElementById("right-search-overlay").style.width = "100%";
+  document.body.style.overflowY = "clip";
+ /* document.body.style.maxHeight = "101vh";*/
+
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeSearch() {
+  document.getElementById("right-search-overlay").style.width = "0%";
+  document.body.style.overflowY = "unset";
+ /* document.body.style.maxHeight = "unset";*/
+} 
+
+ 
+
+/*READ OVERLAY
+
+Opens up the Earthbook overlay from the bottom up*/
+
+
+  /* Open settings when someone clicks on the span element */
+  function openRead() {
+    document.getElementById("read-curtain").style.height = "101vh";
+    document.body.style.overflowY = "hidden";
+    document.body.style.maxHeight = "101vh";
+  }
+
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeRead() {
+    document.getElementById("read-curtain").style.height = "0%";
+    document.body.style.overflowY = "unset";
+    document.body.style.maxHeight = "unset";
+  } 
+
+
+
+
+
+
 /* BOTTOM COMMENTS OVERLAY 
 
 Triggers the full screen chapter comments*/
@@ -118,201 +227,6 @@ function closeComments() {
   document.getElementById("right-close-button2").style.position = "absolute";
 
 } 
-
-
-
-
-/* BOOKPLATE OVERLAY
-
-Opens the bookplate overlay from the top up*/
-
-function openBookplate() {
-  document.getElementById("top-bookplate").style.height = "100%";
-  document.body.style.overflowY = "hidden";
- // document.body.style.maxHeight = "101vh";
-  document.getElementById("sub-earthbook").style.display = "block!important";
-  document.getElementById("bookplate-img").style.display = "block";
-
-  var modal = document.getElementById('top-bookplate');
-
-function modalShow () {
-   modal.setAttribute('tabindex', '0');
-   modal.focus();
-}
-
-function focusRestrict ( event ) {
-  document.addEventListener('focus', function( event ) {
-    if ( modalOpen && !modal.contains( event.target ) ) {
-      event.stopPropagation();
-      modal.focus();
-    }
-  }, true);
-}
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay 
-
-NOT YET TRANSFRERED TO ESCAPE SCRIPT*/
-
-function closeBookplate() {
-  document.getElementById("top-bookplate").style.height = "0%";
-  document.body.style.overflowY = "unset";
-  //document.body.style.maxHeight = "unset";
-  document.getElementById("sub-earthbook").style.display = "none";
-  document.getElementById("bookplate-button").style.display = "none";
-  document.getElementById("bookplate-img").style.display = "none";
-
-} 
-
-
-
-/* RIGHT SEARCH OVERLAY 
-
-Triggers the right search panel*/
-
-function openSearch() {
-  document.getElementById("right-search-overlay").style.width = "100%";
-  document.body.style.overflowY = "clip";
- /* document.body.style.maxHeight = "101vh";*/
-
- var modal = document.getElementById('right-search-overlay');
-
-function modalShow () {
-   modal.setAttribute('tabindex', '0');
-   modal.focus();
-}
-
-function focusRestrict ( event ) {
-  document.addEventListener('focus', function( event ) {
-    if ( modalOpen && !modal.contains( event.target ) ) {
-      event.stopPropagation();
-      modal.focus();
-    }
-  }, true);
-}
-
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeSearch() {
-  document.getElementById("right-search-overlay").style.width = "0%";
-  document.body.style.overflowY = "unset";
- /* document.body.style.maxHeight = "unset";*/
-} 
-
- 
-
-
-
-
-/*READ OVERLAY
-Opens up the Earthbook overlay from the bottom up*/
-
-  /* Open settings when someone clicks on the span element */
-  function openRead() {
-    document.getElementById("read-curtain").style.height = "101vh";
-    document.body.style.overflowY = "hidden";
-    //document.body.style.maxHeight = "101vh";
-
-    var modal = document.getElementById('read-curtain');
-
-    function modalShow () {
-       modal.setAttribute('tabindex', '0');
-       modal.focus();
-    }
-    
-    function focusRestrict ( event ) {
-      document.addEventListener('focus', function( event ) {
-        if ( modalOpen && !modal.contains( event.target ) ) {
-          event.stopPropagation();
-          modal.focus();
-        }
-      }, true);
-    }
-    }
-  
-
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeRead() {
-    document.getElementById("read-curtain").style.height = "0%";
-    document.body.style.overflowY = "unset";
-    //document.body.style.maxHeight = "unset";
-  } 
-
-
-
-
-/* RIGHT SHARE OVERLAY 
-
-Triggers the right share link panel*/
-
-function openShare() {
-  document.getElementById("right-share-overlay").style.width = "100%";
-  document.body.style.overflowY = "hidden";
-  //document.body.style.maxHeight = "101vh";
-
-  var modal = document.getElementById('right-share-overlay');
-
-function modalShow () {
-   modal.setAttribute('tabindex', '0');
-   modal.focus();
-}
-
-function focusRestrict ( event ) {
-  document.addEventListener('focus', function( event ) {
-    if ( modalOpen && !modal.contains( event.target ) ) {
-      event.stopPropagation();
-      modal.focus();
-    }
-  }, true);
-}
-}
-
-
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeShare() {
-  document.getElementById("right-share-overlay").style.width = "0%";
-  document.body.style.overflowY = "unset";
-  //document.body.style.maxHeight = "unset";
-
-} 
-
-
-
-
-/* LEFT SETTINGS OVERLAY */
-function openSettings() {
-  document.getElementById("left-settings-overlay").style.width = "100%";
-  document.body.style.overflowY = "hidden";
- // document.body.style.maxHeight = "101vh";
-
- var modal = document.getElementById('left-settings-overlay');
-
-function modalShow () {
-   modal.setAttribute('tabindex', '0');
-   modal.focus();
-}
-
-function focusRestrict ( event ) {
-  document.addEventListener('focus', function( event ) {
-    if ( modalOpen && !modal.contains( event.target ) ) {
-      event.stopPropagation();
-      modal.focus();
-    }
-  }, true);
-}
-}
-
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeSettings() {
-  document.getElementById("left-settings-overlay").style.width = "0%";
-  document.body.style.overflowY = "unset";
- // document.body.style.maxHeight = "unset";
-  //document.body.style.height = "unset";
-} 
-
 
 
 /* TABLE OF CONTENTS OVERLAY
@@ -472,9 +386,11 @@ function modalCloseComments ( e ) {
   if ( !e.keyCode || e.keyCode === 27 ) {
     document.getElementById("bottom-comments-overlay").style.height = "0%";
     document.getElementById("eco-curtain2").style.height = "0%";
-    document.getElementById("right-close-button2").style.position = "absolute";
-    document.getElementById("bottom-earthbook").style.height = "0%";
+  document.body.style.overflowY = "unset";
+  document.getElementById("right-close-button2").style.position = "absolute";
 
+  document.getElementById("bottom-earthbook").style.height = "0%";
+    /*document.body.style.maxHeight = "unset";*/
     document.getElementById("medium-message").style.transitionDelay = "2s";
     document.getElementById("medium-message").style.opacity = "0";
 
@@ -486,12 +402,7 @@ function modalCloseComments ( e ) {
   document.getElementById("sub-earthbook").style.display = "none";
   document.getElementById("bookplate-button").style.display = "none";
   document.getElementById("bookplate-img").style.display = "none";
-  document.getElementById("left-settings-overlay").style.width = "0%";
-  document.getElementById("right-share-overlay").style.width = "0%";
-  document.getElementById("read-curtain").style.height = "0%";
-  document.getElementById("right-search-overlay").style.width = "0%";
-
-
+  
   }
 }
 document.addEventListener('keydown', modalCloseComments);
