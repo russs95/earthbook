148ca74d6665a0e76bde91b7af4b4154e727aca3
Russell Maier <russ@earthen.io>
2023-02-26 11:54:18 +0800

    back?

--- 404.php
+++ 404.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
+<!--EARTHBOOK CHAPTER PAGE TEMPLATE
 
-<!--EARTHBOOK - An open source, Earth & Human Friendly Book format
-Read the book.  Improve, translate or comment on the content.  Or fork the code and publish your own.  
-
-Chapter Template Version 1.1
-Created by Russell Maier for the Tractatus Ayyew: Earthen Ethics.
-
+PHP Page Version 1.0.2
+Design by Russell Maier
 License: GNU General Public License v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 All files, unless otherwise stated, are released under the GNU General Public
@@ -15,34 +11,140 @@ See full project repository at: https://github.com/russs95/earthbook
 -->
 
 
-<!-- this grabs the language identifier for the page so that it can used in the meta and canonical url variables.  It also grabs the page name.  manifest="../offline.appcache-->
+<!-- PHP starts by laying out canonical URLs for the page and language -->
 
-<?php require_once ("lang.php");
+<!DOCTYPE html>
 
-echo <<<_END
-<html lang="$lang">
-_END;?>
+ 
 
-<!-- this sets PHP variables for the pages that will be used later on-->
-
-<?php 
-$parts = explode ("/", $_SERVER['SCRIPT_NAME']);
-$name = $parts [count($parts)-1];
-if (strcmp($name, "index.php") == 0)
-$name = "";
-$page_number = "101";
-$page_title = "404 Page not Found | Tractatus Ayyew - An Earthbook";?>
-
-<head>
+<!-- this grabs the language identifier for the page so that it can used in the meta and canonical url variables.  It also grabs the page name.-->
+<html lang="en" manifest="../offline.appcache">
+<head><meta charset="utf-8">
 
 
-<?php require_once ("../header.php");?>
+<link rel="canonical" href="https://book.earthen.io/en/404.php"> 
+<link rel="alternate" href="https://book.earthen.io/en/404.php" hreflang="en" />
+<link rel="alternate" href="http://book.earthen.io/en/404.php" hreflang="x-default" />
+<!--<link rel="alternate" href="https://book.earthen.io/fr/404.php" hreflang="fr" />
+<link rel="alternate" href="https://book.earthen.io/es/404.php" hreflang="es" />
+<link rel="alternate" href="https://book.earthen.io/id/404.php" hreflang="id" />-->
 
+
+ 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="article:modified_time" content="2022-12-03T09:14:13+00:00" />
+
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-title" content="Tractatus Ayyew - EarthBook">
+<meta name="apple-mobile-web-app-status-bar-style" content="pink">
+
+
+<!--BOOK META TAGS var(--header-footer);
+These tags will be consistent for the whole book-->
+
+<meta property="og:site_name" content="Tractatus Ayyew" />
+<meta property="article:publisher" content="Earthen.io" />
+<meta name="author" content="Russell Maier & Banayan Angway">
+<meta property="og:type" content="book" />
+<link rel="apple-touch-icon" sizes="180x180" href="../favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../favicons/favicon-16x16.png">
+<link rel="manifest" href="../favicons/site.webmanifest">
+
+<meta name="twitter:card" content="summary" />
+<meta property="og:type"          content="book">
+<meta property="og:image"         content="https://book.earthen.io/covers/earth-book-cover-1000px.jpg"/>
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="1000px" />
+<meta property="og:image:height" content="1500px" />
+
+<meta property="og:locale" content="en" />
+
+<meta property="og:url" content="https://tractatus.earthen.io/en/404.php">
+
+
+ <!-- Fonts
+The requisite typography for the page-->
+
+<link rel="preconnect" href="../fonts/">
+<link rel="preconnect" href="../fonts/" crossorigin> 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<!-- Mulish loads first for first page view-->
+
+<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500&display=swap" rel="stylesheet" media="print" onload="this.media='all'"> 
+
+	
+<!--Preload graphics-->
+<link rel="preload" as="image" href="../svgs/footer-settings-button.svg">
+<link rel="preload" as="image" href="../svgs/footer-settings-button-over.svg">
+<link rel="preload" as="image" href="../svgs/footer-settings-button-up.svg">
+
+ <!-- Style Sheets   
+All the css needed for this page-->
+ 
+ 
+ <!--<link  rel="stylesheet" type="text/css" href="../style-sheet.css?v1.3">
+<link rel="stylesheet" href="../acessibilidade.css" />
+<link rel="stylesheet" type="text/css" href="../stylesheet-chapter.css">-->
+
+<link rel="preload" href="../style-sheet.css?v2.01" as="style" onload="this.rel='stylesheet'">
+<link rel="preload" href="../light.css?v1.8" as="style" onload="this.rel='stylesheet'">
+<link rel="preload" href="../acessibilidade.css" as="style" onload="this.rel='stylesheet'">
+<link rel="preload" href="../stylesheet-chapter.css" as="style" onload="this.rel='stylesheet'">
+<link rel="preload" as="image" href="../svgs/up-arrow-dark.svg">
+<link rel="preload" as="image" href="../svgs/up-arrow-light.svg">
+
+<link rel="stylesheet" href="../light.css?v1.8" media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)">
+ <link rel="stylesheet" href="../dark.css?v1.8" media="(prefers-color-scheme: dark)">
+ 
+ <!-- INCLUDES
+ Main Scripts-->   
+
+ <script src="../content-javascript.js" defer></script>
+ <script src="../universal-javascript.js" defer></script>
+ <script src="../clip/dist/clipboard.min.js"></script>
+ <script src="../accessibility.js" defer></script>
+ <script type="module" src="https://unpkg.com/dark-mode-toggle" async></script>
+ 
+ <!-- Arc CDN Script without delay
+<script async src="https://arc.io/widget.min.js#Z7EC7Cze"></script>-->
+
+
+
+ <!--Page Components
+All the page components required to build this page-->
+
+ <!--1--><script src="includes/header-component.js" type="text/javascript" preload></script>
+ <!--2--><script src="includes/settings-curtain.js" type="text/javascript" defer></script>
+ <!--3--><script src="includes/content-curtain.js" type="text/javascript" defer></script>
+ <!--4--><script src="includes/share-curtain.js" type="text/javascript" defer></script>
+ <!--5--><script src="includes/buy-curtain.js" type="text/javascript" defer></script>
+ <!--6--><script src="includes/bookplate-curtain.js" type="text/javascript" defer></script>
+ <!--7--><script src="includes/earthbook-curtain.js" type="text/javascript" defer></script>
+ <!--8b--><script src="includes/footer-chapter.js" type="text/javascript" defer></script>
+
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-G5VT56C7XD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-G5VT56C7XD');
+</script>
+
+
+
+<!--END OF GENERIC CONTENT-->
 
 <!--META TAGS
 Must be updated for each page-->
 
-<meta property="article:modified_time" content="2023-02-25T09:14:13+00:00">
+<title>404 Page not found | Tratatus Ayyew - Earthbook</title>
 <meta name="keywords" content="not yet published, epigraph, introduction, stories, dapay, Earth ethics, earthen ethics, earth, ethics, tractatus ayyew, igorot, indigenous philosophy, philosophy, Earth, green ethics, environmental ethics, deep ecology, ecological, ethics"> 
 <meta name="description" content="This chapter hasn't yet been published.">
 
@@ -50,7 +152,9 @@ Must be updated for each page-->
 <meta name="twitter:label1" content="Est. reading time" />
 <meta name="twitter:data1" content="1 minutes" /> 
 	
-<meta property="og:description"   content="We couldn't find the page you were looking for."/>
+<!-- Facebook Open Graph Tags for social sharing-->
+<meta property="og:title"         content="Not yet published | Tratatus Ayyew - Earthbook.">
+<meta property="og:description"   content="This chapter hasn't yet been published."/>
 
 
 
@@ -99,58 +203,85 @@ Must be updated for each page-->
   #ct-main .action-btn {
     margin: -35px auto 0px auto !important;
   }
-  #ct-chapter-title {
-  
-  font-size: 10vmin;
-  letter-spacing: 1px;
-}
 
 </style>
+
 
 </head>
 
 
-<!--MAIN HTML Begins-->
+<!--Main HTML Begins-->
 
-<BODY class="accessibility-plugin-ac" style="max-width:100%; overflow-x:hidden;">
-<div id="underlayer">
+<BODY class="accessibility-plugin-ac">
+  <div id="underlayer">
 
-<div id="top"></div>
+    <a name="top"></a>
 
 <!--HEADER NAVBAR-->
 <header-component></header-component>
 
 <div id="ct-chapter-top">
-<button type=button id="ct-tc-menu" onclick="openContents()" aria-label="Open Table of Contents"></button>
-<div class="ct-holder" style="cursor:pointer;" onclick="openContents()">
-    <div id="ct-chapter-title">404 Error</div>   
-      <div id="ct-book-title">Page Not Found<!--#echo var="REDIRECT_STATUS" --></div>
+    <div id="ct-tc-menu" onclick="openContents()"></div>
+    <div class="sero" style="cursor:pointer;" onclick="openContents()">
+      <div id="ct-chapter-title">Page not found</div>   
+      <div id="ct-book-title">EarthBook Error <!--#echo var="REDIRECT_STATUS" --></div>
       <div id="ct-chap-location">Tractatus Ayyew</div>
       <div id="ct-word-count"><i>An Earthen Ethics</i></div>
     </div>
 </div>
 
  
-<?php require_once ("includes/eco-curtain.php");?>
- 
+
+<!--CONTENTS MENU-->
+<content-curtain></content-curtain>
+
+<!--SETTINGS-MENU-->
+<settings-curtain></settings-curtain>
+
+<!--BOOK PLATE-->
+<bookplate-curtain></bookplate-curtain>
+
+<!--ABOUT EARTHBOOK-->
+<earthbook-curtain></earthbook-curtain>
+
+<!--SHARE-MENU-->
+<share-curtain></share-curtain>
+
+<!--BUY-CURTAIN-->
+<buy-curtain></buy-curtain>
+
+<!--ECO-CURTAIN-->
+
+
+<div id="eco-curtain2" class="eco-overlay">
+  <div class="overlay-content-eco">         
+        
+    <div class="eco-header">The Earthbook format is hand-coded from the ground up to be green.</div>
+
+    <div class="eco-text">
+    In an Earthbook's writing & publishing, its carbon, plastic and biodiversity impacts are tracked, accounted and disclosed.  See this Earthbook's full <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank" aria-label="The Regen Report for our book">Regenerativity Report.</a>
+    </div>
+
+    <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank" aria-label="The Regen Report for our book">
+    <div><img src="../banners/forearth-dark-350px.png" width="300px" alt="Banner declaring our For-Earth intention"></div></a>
+
+     <div id="wcb" class="carbonbadge wcb-d"></div>
+    <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js?v=2" preload></script>
+
+    <div class="eco-medium">The Medium is the Message.</div>
+    <span style="cursor:pointer" onclick="closeEco()" aria-label="Close">
+        <div id="bottom-close-button">
+            <img src="../svgs/bottom-x.svg" alt="Close button">
+        </div>
+    </span>
+  </div>
+</div> 
 <!-- Page Title Section-->
 
 
 <div id="ct-chapt-graphic" class="background-000">
     <div class="ct-chapter-quote">
-    The requested page  <?php
-echo $_SERVER['PHP_SELF'];
-echo "<br>";
-echo $_SERVER['SERVER_NAME'];
-echo "<br>";
-echo $_SERVER['HTTP_HOST'];
-echo "<br>";
-echo $_SERVER['HTTP_REFERER'];
-echo "<br>";
-echo $_SERVER['HTTP_USER_AGENT'];
-echo "<br>";
-echo $_SERVER['SCRIPT_NAME'];
-?>  couldn't be matched with any other in the EarthBook.  Check the URL for errors or mispellings.
+    The requested page (<!--#echo var="REQUEST_URI" -->) couldn't be matched with any other in the EarthBook.  Check the URL for errors or mispellings.
     </div>
 </div>
 
@@ -177,8 +308,35 @@ echo $_SERVER['SCRIPT_NAME'];
 
     <br><br>
 
-    <?php require_once ("../manage.php");?>
+      <!--<!DOCTYPE html>
+  <html>
+  <head>
+  <title>Subscribe/Unsubscribe to a Mailing List</title>
+  </head>
+  <body>-->
+ 
+    <form method="POST" action="/en/404.php">
 
+   <!--<p><label for="email">Your E-Mail Address:</label><br/>-->
+  <div class="this-one"><input type="email" id="email" name="email"  placeholder="Want more? Enter email..."
+         size="40" maxlength="150"  /></p></div>
+
+  
+  <button type="submit" name="submit"  value="submit" class="action-btn" style="width: 65%;
+ 
+  font-size: 0.9em;
+  margin: -25px auto 0px auto;
+  border-radius: 0px 0px 20px 20px;">Register</button>
+  <fieldset>
+  <input type="radio" id="action_sub" name="action"
+         value="sub" checked />
+  <label for="action_sub">subscribe</label><br/>
+  <input type="radio" id="action_unsub" name="action"
+         value="unsub" />
+  <label for="action_unsub">unsubscribe</label>
+  </fieldset>
+  </form> <!--</body>
+ </html>-->
     <br>
 </div>
 
@@ -217,7 +375,52 @@ echo $_SERVER['SCRIPT_NAME'];
   </div>
 </div>
 
-<?php require_once ("includes/chap-footer.php");?>
+
+  
+<div id="footer-settings">
+
+<div class="footer-size" style="height:70px;">
+
+  <div class="footer-settings-icon" style="cursor:pointer" aria-label="Click to return to the homepage" ><a href="index.php"><img src="../icons/home.svg" alt="home page icon"></a></div>
+
+  <div class="footer-settings-icon" style="cursor:pointer" onclick="openBookplate()" aria-label="Click to open the bookplate"><img src="../svgs/my-book-header-logo-grey.svg" title="Tractatus Ayyew bookplate" alt="Tractatus Ayyew Mandala"></div>
+  
+  <div class="footer-settings-icon"><img src="../icons/buy.svg" style="cursor:pointer" onclick="openBuy()" alt="Buy icon" aria-label="Click to go to ebook purchase page"></div>
+
+  <div class="footer-settings-icon"><img src="../icons/settings.svg" alt="Settings icon" style="cursor:pointer" onclick="openSettings()" aria-label="Click to adjust book reading settings"></div>
+
+  <div class="footer-settings-icon"><img src="../icons/share.svg" alt="share icon" style="cursor:pointer" onclick="openShare()" aria-label="Click to copy the link of the earthBook to share"></div>
+
+  <div class="footer-settings-icon"><img src="../icons/eco.svg" alt="Eco Accounting icon" style="cursor:pointer" onclick="openEco()" aria-label="Click to view CO2 site audit"></div>
+
+  <div class="footer-settings-icon"><img src="../icons/content.svg" alt="Table of Contents icon" style="cursor:pointer" onclick="openContents()" aria-label="Click to view EarthBook Table of Contents"></div>
+
+ 
+ 
+
+  <div class="footer-settings-icon" style="flex-grow:4; align-self:center;"> <a href="#top"><img src="../svgs/to-the-top-arrow.svg" alt="To the top of the page" title="To the top of the page" style="cursor:pointer;" aria-label="Go to the top of the page"></div></a>
+
+</div>
+
+<div class="footer-size" style="height:30px;">
+
+  <div class="footer-settings-icon" style="cursor:pointer; text-align:left; flex-grow:1; padding-right:8px; padding-top: 20px;
+  padding-bottom: 0px;" onclick="openEarthbook()" aria-label="Learn what an Earthbook is">What is an Earthbook?</a></div>
+
+  <div class="github-source">Page source code on Github:<br>
+  <a href="https://github.com/russs95/earthbook/blob/main/en/404.php " target="_blank" aria-label="Go to the Earthbook Github repository">../earthbook/blob/main/en/404.php ➔</a></div>
+</div>
+
+
+
+</div>
+
+</div>
+  <div id="containerSepia"></div>
+  <div id="containerContrast"></div>
+  <div id="containerBrightness"></div>
+  
+</div>
 
 
 </body>
