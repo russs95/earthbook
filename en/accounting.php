c2e663e488a5e3121af538dab4e1311a152b1167
Russell Maier <russ@earthen.io>
2023-03-01 11:11:24 +0800

    two

--- en/accounting.php
+++ en/accounting.php
@@ -168,7 +168,7 @@ Must be updated for each page-->
 <div class="lead-page-paragraph">
 
   <p>
-    The ecological impact of the printing, publishing and reading of the <i>Tractatus Ayyew</i> is tracked, accounted and disclosed.  Download our annual Regeneral Reports here:
+    The ecological impact of the printing, publishing and reading of the <i>Tractatus Ayyew</i> is tracked, accounted and disclosed.  Download our annual Regeneral Reports here:<br><br>
   </p>
 
   </div>
@@ -184,7 +184,12 @@ border-radius: 12px;flex-direction:row;border: 1px dashed #80808061;">
       
       <div class="doc-texts" style="display:flex;flex-direction:column;justify-content:center;padding-right: 20px;">
         <div class="doc-text-main"><a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank" aria-label="The Regen Report for our book" alt="Banner declaring our For-Earth intention">Tracatus Ayyew Writing & Publishing</a></div>
-        <div class="doc-text-small"><a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank"><b>2022 Regen Report | PDF |  554kb</b></a></div>
+        <div class="doc-text-small"><a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank"><b>2021 Regen Report</b> | PDF |  554kb | Issued 12/02/2022</a></div>
+  </div>
+
+  <div class="doc-texts" style="display:flex;flex-direction:column;justify-content:center;padding-right: 20px;">
+        <div class="doc-text-main"><!--<a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank" aria-label="The Regen Report for our book" alt="Banner declaring our For-Earth intention">Tracatus Ayyew Writing & Publishing</a>--></div>
+        <div class="doc-text-small"><!--<a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank">--><b>2022 Regen Report</b> | PDF | Feb 2023 release pending</b></a></div>
   </div>
   <br><br><br><br>
 
