1cf627782b0407e2fd282b55831a500a15afa5e2
Russell Maier <russ@earthen.io>
2023-03-23 12:56:40 +0800

    fix that

--- en/includes/share-curtain.js
+++ en/includes/share-curtain.js
@@ -101,9 +101,6 @@ function confirmCopy() {
   }
 }
 
-function getMainurl() {
-  document.getElementById("page-url").innerHTML = 
-"Banayan Angway, Russell Maier, 'Tractatus Ayyew: An Earthen Ethics' (Earthen.io, Kalinga, Philippines & Bali, Indonesia, 2022). <?php echo ($chap_number); echo ($chap_name); ;?>";
-}
+
 
 
