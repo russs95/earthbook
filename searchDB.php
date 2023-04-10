baf96f47cfcf06ee464c657fcb631c58840d682d
Russell Maier <russ@earthen.io>
2023-04-10 20:16:08 +0800

    error reporting

--- searchDB.php
+++ searchDB.php
@@ -1,5 +1,9 @@
 <?php
-error_reporting(E_ALL); ini_set('display_errors', '1');
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 // search query
 $search = $_GET["search"];
