32614eb90b8efe5bc2386445c2e7ff425ff2fb0a
Russell Maier <russ@earthen.io>
2023-05-05 17:46:58 +0800

    no ;

--- en/includes/content-curtain.js
+++ en/includes/content-curtain.js
@@ -11,7 +11,7 @@ class ContentCurtain extends HTMLElement {
     <div id="top-close-button">
         <a href="index.php"><img src="../icons/home.svg" alt="Home Button" style="padding-right:10px;padding-left:10px;"></a> 
         <img src="../svgs/bottom-x.svg" alt="Close Button" style="padding-left:10px;cursor:pointer" onclick="closeContents()">
-        <img src="../icons/question.svg" alt="Guided Tour" style="padding-left:10px;padding-right:15px;cursor:pointer" onclick="closeContents();, guidedTour();">
+        <img src="../icons/question.svg" alt="Guided Tour" style="padding-left:10px;padding-right:15px;cursor:pointer" onclick="closeContents(), guidedTour()">
     </div>
         
         <div class="overlay-content-tc">
