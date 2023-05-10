6aa0db4a1b21c01ed217d0034b5f8418018f0e17
Russell Maier <russ@earthen.io>
2023-05-10 11:25:30 +0800

    once more

--- universal-javascript.js
+++ universal-javascript.js
@@ -538,6 +538,10 @@ Here are all the scripts useScripts used on all Earthbook pages to pull in the v
  function EarthbookSearch() {
   // Get the search query from the input field
   var query = document.getElementById("search_input").value.toLowerCase();
+  var overlayContent = document.querySelector('.search-overlay-content');
+  overlayContent.style.height = 'fit-content';
+
+
 
   // Load the JSON file
   var xmlhttp = new XMLHttpRequest();
