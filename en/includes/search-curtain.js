31977e726db29f69bfa873516dfc9db944442f21
Russell Maier <russ@earthen.io>
2023-05-12 19:32:12 +0800

    retry

--- en/includes/search-curtain.js
+++ en/includes/search-curtain.js
@@ -21,10 +21,10 @@ class SearchCurtain extends HTMLElement {
             <div class="search-box" style="padding-bottom:15%;">
       <div class="copy-section">
         <input id="search_input" type="text" placeholder="Enter search term..." aria-label="Enter keywords...">   
-        <button class="btn" style="padding:22px 0px 22px 0px;" onclick="EarthbookSearch(['chapter-index.json', 'glossary.json'])"
+        <button class="btn" style="padding:22px 0px 22px 0px;" onclick="EarthbookSearch(['chapters-index.json', 'glossary.json'])"
         aria-label="Search Button"><i style="background-image: url(../icons/search-white.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 2px;"></i></button> 
       </div>  
-      <div id="search_results"><h6>Glossarry keyword definitions are shown first</h6></div>
+      <div id="search_results"><h6>Glossarry definitions are shown first.</h6></div>
     </div>
     
           </div>
