f6905d5d0e98a9c95d6017aeafa5b1c35d704e68
Russell Maier <russ@earthen.io>
2023-05-14 16:08:30 +0800

    bottom

--- en/includes/chapTER-footer.php
+++ en/includes/chapTER-footer.php
@@ -258,6 +258,37 @@ function clearResults() {
 
 </script>
 
+<script>
+  // Function to adjust the font size
+function adjustFontSize(className, change) {
+  const body = document.querySelector('body');
+  const elements = body.querySelectorAll(`.${className}`);
+
+  elements.forEach(element => {
+    let fontSize = window.getComputedStyle(element).getPropertyValue('font-size');
+    fontSize = parseFloat(fontSize);
+
+    if (change === 'increase') {
+      fontSize += 1;
+    } else if (change === 'decrease') {
+      fontSize -= 1;
+    } else if (change === 'normal') {
+      fontSize = "unset";
+    }
+
+    element.style.fontSize = `${fontSize}px`;
+    localStorage.setItem('UserSetFontSize', `${fontSize}px`);
+  });
+}
+
+// Set the font size on page load
+window.onload = () => {
+  const userSetFontSize = localStorage.getItem('UserSetFontSize');
+  if (userSetFontSize) {
+    adjustFontSize('user-set-font-size', 'set', userSetFontSize);
+  }
+}
+
 
 
   </script>
--- en/index.php
+++ en/index.php
@@ -597,7 +597,39 @@ function clearResults() {
   document.querySelector("link[rel='alternate'][hreflang='es']").setAttribute("href", "https://book.earthen.io/es/" + name);
   document.querySelector("link[rel='alternate'][hreflang='id']").setAttribute("href", "https://book.earthen.io/id/" + name);
 }
-    </script>
+    
+    
+function adjustFontSize(className, change) {
+  const body = document.querySelector('body');
+  const elements = body.querySelectorAll(`.${className}`);
+
+  elements.forEach(element => {
+    let fontSize = window.getComputedStyle(element).getPropertyValue('font-size');
+    fontSize = parseFloat(fontSize);
+
+    if (change === 'increase') {
+      fontSize += 1;
+    } else if (change === 'decrease') {
+      fontSize -= 1;
+    } else if (change === 'normal') {
+      fontSize = "unset";
+    }
+
+    element.style.fontSize = `${fontSize}px`;
+    localStorage.setItem('UserSetFontSize', `${fontSize}px`);
+  });
+}
+
+// Set the font size on page load
+window.onload = () => {
+  const userSetFontSize = localStorage.getItem('UserSetFontSize');
+  if (userSetFontSize) {
+    adjustFontSize('user-set-font-size', 'set', userSetFontSize);
+  }
+}
+
+
+</script>
 
 
 
