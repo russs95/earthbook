aedcb73185bafac32e9070c8bbea101822f2fb33
Russell Maier <russ@earthen.io>
2023-06-03 08:52:04 +0800

    service 2

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('earthbook-cache').then(function(cache) {
+    caches.open('earthbook-cache').then(cache => {
       return cache.addAll([
         '/',
         '/index.php',
@@ -71,10 +71,23 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    caches.match(event.request).then(response => {
+      // If the requested URL is in the cache, return the cached response
+      if (response) {
+        return response;
+      }
+
+      // If the requested URL is not in the cache, fetch it from the network
+      return fetch(event.request).then(response => {
+        // Clone the response to cache it and then return the response
+        const clonedResponse = response.clone();
+        caches.open('my-cache').then(cache => {
+          cache.put(event.request, clonedResponse);
+        });
+        return response;
+      });
     })
   );
 });
