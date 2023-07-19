1fb9adc480bcd7ab608002e2a4e627ed397ad962
Russell Maier <russ@earthen.io>
2023-07-19 12:56:08 +0800

    double cache

--- service-worker.js
+++ service-worker.js
@@ -62,6 +62,29 @@ self.addEventListener('install', event => {
   );
 });
 
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // If the requested URL is in the cache, return the cached response
+      if (response) {
+        return response;
+      }
+
+      // If the requested URL is not in the cache, fetch it from the network
+      return fetch(event.request).then(response => {
+        // Clone the response to cache it and then return the response
+        const clonedResponse = response.clone();
+        caches.open('my-cache').then(cache => {
+          cache.put(event.request, clonedResponse);
+        });
+        return response;
+      });
+    })
+  );
+});
+
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
