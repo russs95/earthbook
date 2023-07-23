5e04422dfeb9fd2527e3291e279d3a79a2626399
Russell Maier <russ@earthen.io>
2023-07-23 17:47:48 +0800

    d

--- en/includes/eco-curtain.js
+++ en/includes/eco-curtain.js
@@ -7,30 +7,30 @@ class EcoCurtain extends HTMLElement {
     this.innerHTML = `
  
 
+    <div id="eco-curtain2" class="eco-overlay">
+    <div class="overlay-content-eco">         
+          
+      <div class="eco-header">The Earthbook format is hand-coded from the ground up to be green.</div>
   
-
-<div id="eco-curtain2" class="eco-overlay">
-<div class="overlay-content-eco">         
+      <div class="eco-text">
+      In an Earthbook's writing & publishing, its carbon, plastic and biodiversity impacts are tracked, accounted and disclosed.  See this Earthbook's full <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank" aria-label="The Regen Report for our book">Regenerativity Report.</a>
+      </div>
+  
+      <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank" aria-label="The Regen Report for our book">
+      <div><img src="../banners/forearth-dark-350px.png" alt="Banner declaring our For-Earth intention" style="width:300px;"></div></a>
+  
+       <div id="wcb" class="carbonbadge wcb-d"></div>
+      <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js?v=2" defer></script>
+  
+      <div class="eco-medium">The Medium is the Message.</div>
+  
       
-  <div class="eco-header">The Earthbook format is hand-coded from the ground up to be green.</div>
-
-  <div class="eco-text">
-  In an Earthbook's writing & publishing, its carbon, plastic and biodiversity impacts are tracked, accounted and disclosed.  See this Earthbook's full <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank" aria-label="The Regen Report for our book">Regenerativity Report.</a>
+              <button type="button" class="bottom-close-button" style="cursor:pointer" onclick="closeBuy()" aria-label="Click to close this pop-up page">
+                <img src="../svgs/bottom-x.svg" alt="Close page button">
+              </button>
+            </div>
+    
   </div>
-
-  <a href="https://files.earthen.io/s/ioobYWRRKGEpPp4" target="_blank" aria-label="The Regen Report for our book">
-  <div><img src="../banners/forearth-dark-350px.png" alt="Banner declaring our For-Earth intention" style="width:300px;"></div></a>
-
-   <div id="wcb" class="carbonbadge wcb-d"></div>
-  <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js?v=2" defer></script>
-
-  <div class="eco-medium">The Medium is the Message.</div>
-
-    <button type="button" class="bottom-close-button" style="cursor:pointer" onclick="closeEco()" aria-label="Close Modal">
-            <img src="../svgs/bottom-x.svg" alt="Close button 2">
-    </button>
-</div>
-</div>
   
 
     `;
