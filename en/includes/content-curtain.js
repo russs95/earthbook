c76ffaf79458569d5f3245700a0e5119a2bbbede
Russell Maier <russ@earthen.io>
2023-07-26 16:05:39 +0800

    content curtain

--- en/includes/content-curtain.js
+++ en/includes/content-curtain.js
@@ -16,16 +16,21 @@ class ContentCurtain extends HTMLElement {
         
         <div class="overlay-content-tc">
 
+                 
+
                     <div class="contents-title-box">
-                        <div class="contents-title">Books & Contents</div>
-                        <div class="contents-books123">
-                            <div class="tc-a-book" style="border-right:1px gray solid";>One</div>
-                            <div class="tc-a-book" style="border-right:1px gray solid; color:grey;">Two</div>
-                            <div class="tc-a-book" style="border-right:1px gray solid; color:grey;">Three</div>
-                            <div class="tc-a-book" style="color:grey;">Appx</div>
-                        </div>
-                    </div>
-                    <div id="appx-book-1">
+  <div class="contents-title">Books & Contents</div>
+  <div class="contents-books123">
+    <div class="tc-a-book" id="button-one" style="border-right:1px gray solid;">One</div>
+    <div class="tc-a-book" id="button-two" style="border-right:1px gray solid; color:grey;">Two</div>
+    <div class="tc-a-book" id="button-three" style="border-right:1px gray solid; color:grey;">Three</div>
+    <div class="tc-a-book" id="button-appx" style="color:grey;">Appx</div>
+  </div>
+</div>
+
+
+                    <div id="appx-book-one">
+
                         <a href="preamble.php">
                         <div class="tc-item">
                             <div class="chapter-name-tc">Preamble</div>
@@ -97,8 +102,162 @@ class ContentCurtain extends HTMLElement {
                     </div>
 
 
+
+                    <div id="appx-book-two" style="display: none;">
+                        <a href="spirals.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Cycles that Spiral</div>
+                            <div class="word-count-tc"><i>1,682 words</i></div>
+                        </div></a>
+
+                        <a href="preface.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">The Salmon's Spin</div>
+                            <div class="word-count-tc"><i>2,294 words</i></div>
+                        </div></a>
+
+                        <a href="unpublished.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">The Manner of the Mollusk</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>Unpublished</i></div>
+                        </div>
+                        </a>
+
+                        <a href="unpublished.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">The Polyp's Pattern</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>Unpublished</i></div>
+                        </div>
+                        </a>
+
+                        <a href="unpublished.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">The Means of the Myccorhyzal</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>Unpublished</i></div>
+                        </div>
+                        </a>
+
+                    </div>
+
+                    <div id="appx-book-three" style="display: none;">
+                        <a href="unpublished.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Plastic 2.0</div>
+                            <div class="word-count-tc"><i>unpublished</i></div>
+                        </div></a>
+
+                        <a href="unpublished.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Plastic Sequestration</div>
+                            <div class="word-count-tc"><i>unpublished</i></div>
+                        </div></a>
+
+                        <a href="unpublished.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Petro-Capital Transition</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>Unpublished</i></div>
+                        </div>
+                        </a>
+
+                        <a href="unpublished.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Ad Pax Ayyew</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>Unpublished</i></div>
+                        </div>
+                        </a>
+
+                        <a href="unpublished.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">The Myccorhyzal's Means</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>Unpublished</i></div>
+                        </div>
+                        </a>
+
+                    </div>
+
+                    <div id="appx-book-appx" style="display: none;">
+                        
+                        <a href="summaries.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Summaries</div>
+                            <div class="word-count-tc"><i>501</i></div>
+                        </div></a>
+
+                        <a href="dedication.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Dedication</div>
+                            <div class="word-count-tc"><i>26</i></div>
+                        </div></a>
+
+
+                        <a href="for-earth.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">For-Earth Declaration</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>54</i></div>
+                        </div>
+                        </a>
+
+                        <a href="acknowledgements.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Acknowledgements</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>541 words</i></div>
+                        </div>
+                        </a>
+
+                        <a href="publishing.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Publishing</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>133 words</i></div>
+                        </div>
+                        </a>
+
+                        <a href="authors.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Authors</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>218 words</i></div>
+                        </div>
+                        </a>
+
+                        <a href="declaration.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Declarations</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>367 words</i></div>
+                        </div>
+                        </a>
+
+                        <a href="bibliography.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Bibliography</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>740 words</i></div>
+                        </div>
+                        </a>
+
+                        <a href="glossary.php">
+                        <div class="tc-item">
+                            <div class="chapter-name-tc">Glossary</div>
+                            <div class="dotted-line"></div>
+                            <div class="word-count-tc"><i>342 words</i></div>
+                        </div>
+                        </a>
+
+
+                    </div>
+
+
                     <div class="tc-button-row">
-                        <!--<a href="index.html" style="height: 24px; flex-grow: 1"><div id="tour-btn" class="action-btn" ><i style="background-image: url(../icons/tour.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 3px;"></i> Tour</div></a>-->
+                        <a href="index.html" style="height: 24px; flex-grow: 1"><div id="tour-btn" class="action-btn" ><i style="background-image: url(../icons/home.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 3px;"></i></div></a>
 
                         <div id="buy-btn" class="action-btn" onclick="closeContents(), guidedTour()" style="height: 24px;flex-grow: 1"><i style="background-image: url(../icons/tour.svg); width:22px; height:22px;display: inline-block;background-size: 22px;margin-bottom: -3px;margin-right: 5px; cursor:pointer;"></i> Tour</div>
 
@@ -131,3 +290,37 @@ class ContentCurtain extends HTMLElement {
 }
 
 customElements.define('content-curtain', ContentCurtain);
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Show the initial content on page load
+  showContent("one");
+
+  // Add click event listeners to the buttons
+  document.getElementById("button-one").addEventListener("click", function() {
+    showContent("one");
+  });
+
+  document.getElementById("button-two").addEventListener("click", function() {
+    showContent("two");
+  });
+
+  document.getElementById("button-three").addEventListener("click", function() {
+    showContent("three");
+  });
+
+  document.getElementById("button-appx").addEventListener("click", function() {
+    showContent("appx");
+  });
+});
+
+function showContent(content) {
+  // Hide all content divs
+  document.getElementById("appx-book-one").style.display = "none";
+  document.getElementById("appx-book-two").style.display = "none";
+  document.getElementById("appx-book-three").style.display = "none";
+  document.getElementById("appx-book-appx").style.display = "none";
+
+  // Show the selected content div
+  document.getElementById(`appx-book-${content}`).style.display = "block";
+}
+
