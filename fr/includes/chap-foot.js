a561623d454e65108dfb0d4af7338c2e93e2ddc7
Russell Maier <russ@earthen.io>
2023-08-03 20:10:49 +0800

    fr

--- fr/includes/chap-foot.js
+++ fr/includes/chap-foot.js
@@ -8,7 +8,7 @@ class chapfoot extends HTMLElement {
   
       
    
-      <div id="footer-functions-box" style="width:100%; padding-top: 5px;">
+    <div id="footer-functions-box" style="width:100%; padding-top: 5px;">
       <div class="footer-function">
         <div class="footer-function-main" onclick="openCode()" style="z-index:8;">
           <div id="code-down-arrow" style="width: 18px; height: 18px;background: url(../icons/up-arrow.svg) no-repeat center;background-size:contain;margin: auto 15px auto 0;">
@@ -25,16 +25,17 @@ class chapfoot extends HTMLElement {
             Les Earthbook sont open source. Contribuez, corrigez ou bifurquez le code de cet Earthbook sur Github : <a href="https://github.com/russs95/earthbook/blob/main/<?php echo ($lang); ;?>/<?php echo ($name); ;?>" target="_blank" aria-label="Contribuer au code. Accéder au dépôt Github d'Earthbook pour cette page :"><br> ➔ github.com/russs95/earthbook/blob/main/<?php echo ($lang); ;?>/<?php echo ($name); ;?></a>
           </div>
         </div>
-      </div>
       
-      <div class="footer-function">
-        <div class="footer-function-main" style="z-index:10;" onclick="openFooter2()">
-          <div id="footer-arrow" style="width:18px;height:18px;background:url(../icons/down-arrow.svg) no-repeat center; background-size:contain;margin: auto 10px auto 0;"></div>
-          <div class="footer-function-item">
-            <p>Outils et Ressources</p>
-          </div>
-          <div class="footer-function-item-icon">
-            <i style="background-image: url(../icons/settings.svg);"></i>
+      
+        <div class="footer-function">
+          <div class="footer-function-main" style="z-index:10;" onclick="openFooter2()">
+            <div id="footer-arrow" style="width:18px;height:18px;background:url(../icons/down-arrow.svg) no-repeat center; background-size:contain;margin: auto 10px auto 0;"></div>
+            <div class="footer-function-item">
+              <p>Outils et Ressources</p>
+            </div>
+            <div class="footer-function-item-icon">
+              <i style="background-image: url(../icons/settings.svg);"></i>
+            </div>
           </div>
         </div>
       </div>
@@ -42,6 +43,7 @@ class chapfoot extends HTMLElement {
     
     <div id="footer-settings">
       <div class="footer-size" style="height:58px; z-index:0 !important;">
+
         <div style=""><button type="button" class="footer-settings-icon" style="cursor:pointer" aria-label="Retour à la page d'accueil" title="Retour à la page d'accueil" onclick="location.href='index.html'"><img src="../icons/home.svg" alt="icône de la page d'accueil"></button></div>
         
         <div style=""><button type="button" class="footer-settings-icon" style="cursor:pointer" onclick="openBuy()" aria-label="Cliquez pour accéder à la page d'achat"><img src="../icons/buy.svg" alt="icône d'achat" title="Cliquez pour accéder à la page d'achat"></button></div>
@@ -57,16 +59,18 @@ class chapfoot extends HTMLElement {
         <div style=""><button type="button" class="footer-settings-icon"><img src="../svgs/to-the-top-arrow.svg" alt="Revenir en haut de la page" style="cursor:pointer" onclick="location.href='#top'" aria-label="Aller en haut de la page"></button></div>
         
         <div style="flex-grow:4; text-align: right;"><button type="button" class="footer-settings-icon"><img src="../icons/eco-green.svg" alt="Notre suivi, comptabilité et divulgation écologiques" title="Notre suivi, comptabilité et divulgation écologiques" style="cursor:pointer;" aria-label="Ouvrir la comptabilité écologique" onclick="openEco()"></button></div>
+
       </div>
     
       <div class="footer-size" style="height:30px;">
         <div><button type="button" class="footer-settings-icon" style="cursor:pointer; text-align:left; flex-grow:1; padding-right:8px; padding-top: 10px;padding-bottom: 0px;" onclick="openEarthbook()" aria-label="Apprenez ce qu'est un Earthbook">Qu'est-ce qu'un Earthbook ?</button></div>
         <div class="github-source" style="margin-right: 0px;">Le Medium est le Message.</div>
       </div>
-    </div>
 
     </div>
-  </div>
+
+  
+
 
 
 
