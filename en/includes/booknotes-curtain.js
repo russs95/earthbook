ecd966fda6afa0a25eab072cbfa9867e1ef331d1
Russell Maier <russ@earthen.io>
2023-08-09 23:31:30 +0800

    upload!

--- en/includes/booknotes-curtain.js
+++ en/includes/booknotes-curtain.js
@@ -29,11 +29,16 @@ class bookNotes extends HTMLElement {
     </div>
 
         <div style="margin:auto;">
-        <button onclick="downloadBooknotes()">Download Booknotes</button>
+        
+        <button onclick="downloadBooknotes()">Download Booknotes</button><br>
 
             <button id="reset-settings-bn" onclick="resetBookNotes()" style="width: 200px;
         margin: 30px auto 20px auto; cursor:pointer; padding:6px;">No Booknotes Saved</button>
             <div id="instructions-bn" style="font-family:Mulish;color:grey;">Highlight, then click any text in the Earthbook to save it to your Booknotes.</div>
+<br>
+            <input type="file" id="jsonUpload" accept=".json">
+<button onclick="uploadBooknotes()">Upload Booknotes</button>
+
     </div>
 
     <div class="tc-button-row" style="margin-top:auto;">
