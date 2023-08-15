cd2927b201238de60436e501843cb15765a80657
Russell Maier <russ@earthen.io>
2023-08-15 09:33:59 +0800

    !

--- sw.js
+++ sw.js
@@ -1,9 +1,66 @@
-const CURRENT_CACHE = 'earthbook-cache-v3';
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CURRENT_CACHE).then(cache => {
-      return cache.addAll([
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+  };
+  
+  const putInCache = async (request, response) => {
+    const cache = await caches.open("v1");
+    await cache.put(request, response);
+  };
+  
+  const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+    // First try to get the resource from the cache
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      return responseFromCache;
+    }
+  
+    // Next try to use (and cache) the preloaded response, if it's there
+    const preloadResponse = await preloadResponsePromise;
+    if (preloadResponse) {
+      console.info("using preload response", preloadResponse);
+      putInCache(request, preloadResponse.clone());
+      return preloadResponse;
+    }
+  
+    // Next try to get the resource from the network
+    try {
+      const responseFromNetwork = await fetch(request);
+      // response may be used only once
+      // we need to save clone to put one copy in cache
+      // and serve second one
+      putInCache(request, responseFromNetwork.clone());
+      return responseFromNetwork;
+    } catch (error) {
+      const fallbackResponse = await caches.match(fallbackUrl);
+      if (fallbackResponse) {
+        return fallbackResponse;
+      }
+      // when even the fallback response is not available,
+      // there is nothing we can do, but we must always
+      // return a Response object
+      return new Response("Network error happened", {
+        status: 408,
+        headers: { "Content-Type": "text/plain" },
+      });
+    }
+  };
+  
+  // Enable navigation preload
+  const enableNavigationPreload = async () => {
+    if (self.registration.navigationPreload) {
+      await self.registration.navigationPreload.enable();
+    }
+  };
+  
+  self.addEventListener("activate", (event) => {
+    event.waitUntil(enableNavigationPreload());
+  });
+  
+  self.addEventListener("install", (event) => {
+    event.waitUntil(
+      addResourcesToCache([
+          
         
 '/',
 '/index.html',
@@ -78,53 +135,16 @@ self.addEventListener('install', event => {
 'en/declarations.html',
 'en/bibliography.html',
 'en/glossary.html',
-
-
-      ]);
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CURRENT_CACHE) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        return response;
-      }
-
-      return fetch(event.request).then(response => {
-        const clonedResponse = response.clone();
-        caches.open(CURRENT_CACHE).then(cache => {
-          cache.put(event.request, clonedResponse);
-        });
-        return response;
-      }).catch(() => {
-        return caches.match(event.request).then(cachedResponse => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-          throw new Error('Offline: Page not found in cache.');
-        });
-      });
-    })
-  );
-});
-
-
-
-
-
+      ]),
+    );
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      cacheFirst({
+        request: event.request,
+        preloadResponsePromise: event.preloadResponse,
+        fallbackUrl: "/gallery/myLittleVader.jpg",
+      }),
+    );
+  });
