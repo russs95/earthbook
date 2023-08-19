cbe21ed4d496ac8d9b370506813b3417998f5c50
Russell Maier <russ@earthen.io>
2023-08-19 20:23:02 +0800

    arg

--- book-note-scripts.js
+++ book-note-scripts.js
@@ -909,7 +909,7 @@ function viewHighlightInfo(bookNoteId) {
         document.getElementById("publicNote").textContent = `Public note: ${highlight.publicNote}`;
         document.getElementById("userNote").textContent = `Annotation: ${highlight.userNote}`;
         document.getElementById("highlight-viewer").style.display = "block";
-        document.getElementById("bookNoteID").textContent = {bookNoteId};
+        document.getElementById("bookNoteID").textContent = highlight.bookNoteId;
         
         // Add the blur class to the underlayer
         document.getElementById("underlayer").classList.add("blur");
